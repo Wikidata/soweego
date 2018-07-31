96f49f0c13f5911141055d0b555c6ebea1d7c097
Massimo Frasson <maxfrax@gmail.com>
2018-07-31 10:10:27 +0200

    Refactor and implementation of first strategy into musicbrainz

---
+++ target_selection/main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+# Launch point for all the subpackage scripts
+
+import targets.musicbrainz.musicbrainz_baseline_matcher
--- target_selection/targets/musicbrainz/musicbrainz_baseline_matcher.py
+++ target_selection/targets/musicbrainz/musicbrainz_baseline_matcher.py
@@ -4,21 +4,38 @@
 import json
 import os
 import csv
+from ..common import matching_strategies
 
-# Wikidata sample loading
-labels_qid = json.load(open('../../wikidata_sample.json'))
+# Utilities functions
+def get_musicbrainz_artists_from_dump(opened_file_dump):
+    """Given an opened musicbrainz(mb) dump, return a dictionary label - mbid"""
+    label_musicbrainz = {}
+    fieldnames = [i for i in range(0, 20)]
 
-# Opens the latest artist dump and creates a dictionary name - id
-label_musicbrainz = {}
-fieldnames = [i for i in range(0, 20)]
-with open('/Users/maxfrax/Desktop/soweego/target_selection/targets/musicbrainz/musicbrainz_dump_20180725-001823/mbdump/artist') as tsvfile:
-    musicbrainz_artists = csv.DictReader(
-        tsvfile, dialect='excel-tab', fieldnames=fieldnames)
+    musicbrainz_artists = csv.DictReader(opened_file_dump, dialect='excel-tab', fieldnames=fieldnames)
+
     for row in musicbrainz_artists:
         # Checks if it's a person
         if row[10] == '1' or '\\N':
             label_musicbrainz[row[2].lower()] = row[0]
         else:
             print(row[10])
-json.dump(label_musicbrainz, open('artists.json', 'w'),
-          indent=2, ensure_ascii=False)
+
+    return label_musicbrainz
+
+# Retrieves the current module directory path
+path = os.path.abspath(__file__)
+dir_path = os.path.dirname(path)
+
+# Wikidata sample loading
+labels_qid = json.load(open('musicians_wikidata_sample.json'))
+
+# Opens the latest artist dump and creates a dictionary name - id
+artists = {}
+with open('%s/musicbrainz_dump_20180725-001823/mbdump/artist' % dir_path) as tsvfile:
+    artists = get_musicbrainz_artists_from_dump(tsvfile)
+    json.dump(artists, open('%s/artists.json' % dir_path, 'w'), indent=2, ensure_ascii=False)
+
+# Applies a matching strategy
+matches = matching_strategies.equal_strings_match((labels_qid, artists))
+json.dump(matches, open('%s/matches.json' % dir_path, 'w'), indent=2, ensure_ascii=False)
