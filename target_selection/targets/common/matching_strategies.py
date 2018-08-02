c3e9e75dbde253cb392707b1acce5d799bca810f
Marco Fossati <fossati@fbk.eu>
2018-08-02 15:02:08 +0200

    fix typo

--- target_selection/targets/common/matching_strategies.py
+++ target_selection/targets/common/matching_strategies.py
@@ -4,7 +4,7 @@
 from collections import defaultdict
 
 def equal_strings_match(databases) -> dict:
-    """Given a touple of dictionaries (string-id), returns the id-id matches if any"""
+    """Given a tuple of dictionaries (string-id), returns the id-id matches if any"""
     # Baseline matcher 1: perfect strings
     # Perfect matches against BNE names
     matched = defaultdict(list)
