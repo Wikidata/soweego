2f28f76a07589258d9b2f195eb62f54ce1a9755a
Massimo Frasson <maxfrax@gmail.com>
2018-08-03 17:48:44 +0200

    Query working for a bucket (no wikipedia articles)

--- soweego/target_selection/musicbrainz/wikidata_sample_additional_info.py
+++ soweego/target_selection/musicbrainz/wikidata_sample_additional_info.py
@@ -4,16 +4,15 @@ import re
 
 path = get_output_path()
 
-labels_qid = json.load(open('../../wikidata/resources/musicians_sample_labels.json'))
-
 def query_info_for(qids_bucket):
     #TODO
     #creating the big query with the optional chain
     properties = json.load(open('resources/properties_mapping.json'))
-    optionals = ""
+    query = "SELECT * WHERE{ VALUES ?id { %s } " % ' '.join(qids_bucket)
     for k, v in properties.items():
-        optionals += 'OPTIONAL { ?id wdt:%s ?%s . }\n' % (v, re.sub('\W', '', k))
-    print(optionals)
+        query += 'OPTIONAL { ?id wdt:%s ?%s . } ' % (v, re.sub('\W', '', k))
+    query += "}"
+    print(query)
     # do it foreach bucket
     #store everything in a file
 
@@ -21,4 +20,14 @@ def query_info_for(qids_bucket):
 #TODO
 #buckets of a hundred ids
 #call the function
-query_info_for(0)
+
+labels_qid = json.load(open('../../wikidata/resources/musicians_sample_labels.json'))
+entities = ["wd:%s"%v for k,v in labels_qid.items()]
+bucket_size = 100
+buckets = [set(entities[i*bucket_size:(i+1)*bucket_size]) for i in range(0, int((len(entities)/bucket_size+1)))]
+query_info_for(buckets[0])
+
+
+###
+# missing wikipedia and wikidata
+###
