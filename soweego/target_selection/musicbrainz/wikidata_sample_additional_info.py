03da84e6d57f71adf934b5755b5841b21edef504
Massimo Frasson <maxfrax@gmail.com>
2018-08-06 16:28:34 +0200

    Added wikidata links to the sample

--- soweego/target_selection/musicbrainz/wikidata_sample_additional_info.py
+++ soweego/target_selection/musicbrainz/wikidata_sample_additional_info.py
@@ -50,6 +50,16 @@ def get_url_formatters_for_properties():
         json.dump(formatters, open(filepath, 'w'), indent=2, ensure_ascii=False)
         return formatters
 
+# Utils
+def get_wikidata_id_from_uri(uri):
+    '''Given a wikidata entity uri, returns only the id'''
+    return re.search(r'\/(\w+)>', uri).group(1)
+
+def stripe_first_last_characters(string):
+    '''Given a string removes the first and the last characters'''
+    return string[1:-1]
+
+
 @click.command()
 def get_wikidata_sample_links():
     '''Creates the JSON containing url - wikidata id'''
@@ -72,7 +82,8 @@ def get_wikidata_sample_links():
         ids_collection = csv.DictReader(common.api_request_wikidata(query_info_for(bucket)), dialect='excel-tab')
         for id_row in ids_collection:
             # Extracts the wikidata id from the URI
-            entity_id = re.search(r'\/(\w+)>', id_row['?id']).group(1)
+            entity_id = get_wikidata_id_from_uri(id_row['?id'])
+            url_id[stripe_first_last_characters(id_row['?id'])] = entity_id
             # Foreach id in the response, creates the full url and adds it to the dict
             for col in ids_collection.fieldnames:
                 if col != '?id' and id_row[col]:
@@ -101,7 +112,8 @@ def get_sitelinks_for_sample():
         articles_collection = csv.DictReader(common.api_request_wikidata(query_wikipedia_articles_for(bucket)), dialect='excel-tab')
         for article_row in articles_collection:
             #TODO id extraction from wikidata uri asa a function
-            site_url = article_row['?article'][1:-1]
-            url_id[site_url] = re.search(r'\/(\w+)>', article_row['?id']).group(1)
+            site_url = stripe_first_last_characters(article_row['?article'])
+            entity_id = get_wikidata_id_from_uri(article_row['?id'])
+            url_id[site_url] = entity_id
     
     json.dump(url_id, open(filepath, 'w'), indent=2, ensure_ascii=False)
