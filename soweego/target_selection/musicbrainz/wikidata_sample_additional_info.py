f963ae9e787cdd0895570cde02c7abe95b937ebe
Massimo Frasson <maxfrax@gmail.com>
2018-08-06 14:42:00 +0200

    Json file containing all the url formatters

--- soweego/target_selection/musicbrainz/wikidata_sample_additional_info.py
+++ soweego/target_selection/musicbrainz/wikidata_sample_additional_info.py
@@ -1,10 +1,13 @@
 import json
 import click
+import csv
+import os
 import re
 from . import common
 
 PATH = common.get_output_path()
 PROPERTIES_MAPPING_PATH = '%s/resources/properties_mapping.json' % common.get_path()
+properties = {'?%s' % re.sub(r'\W', '', k): v  for k, v in json.load(open(PROPERTIES_MAPPING_PATH)).items()}
 
 def query_info_for(qids_bucket):
     """Given a list of wikidata entities returns a query for getting some external ids"""
@@ -12,11 +15,9 @@ def query_info_for(qids_bucket):
     properties = json.load(open(PROPERTIES_MAPPING_PATH))
     query = "SELECT * WHERE{ VALUES ?id { %s } " % ' '.join(qids_bucket)
     for k, v in properties.items():
-        query += 'OPTIONAL { ?id wdt:%s ?%s . } ' % (v, re.sub(r'\W', '', k))
+        query += 'OPTIONAL { ?id wdt:%s ?%s . } ' % (v, k)
     query += "}"
     return query
-    # do it foreach bucket
-    #store everything in a file
 
 def query_wikipedia_articles_for(qids_bucket):
     """Given a list of wikidata entities returns a query for getting wikidata articles"""
@@ -26,13 +27,22 @@ def query_wikipedia_articles_for(qids_bucket):
     query += "}"
     return query
 
-def query_url_formatters_for_properties():
+def get_url_formatters_for_properties():
     """Retrieves the url formatters for the properties listed in properties_mapping.json"""
+    filepath = '%s/url_formatters.json' % common.get_output_path()
 
-    properties = json.load(open(PROPERTIES_MAPPING_PATH))
-    values = ['wd:%s'%v for _, v in properties.items()]
-    query = "SELECT * WHERE { VALUES ?id {%s} . ?id wdt:P1630 ?formatterUrl . }" % ' '.join(values)
-    return query
+    if os.path.isfile(filepath):
+        return json.load(open(filepath))
+    else:
+        formatters = {}
+        for prop_name, prop_id in properties.items():
+            query = "SELECT * WHERE { %s wdt:P1630 ?formatterUrl . }" % ('wd:%s' % prop_id)
+            reader = csv.DictReader(common.api_request_wikidata(query), dialect='excel-tab')
+            for r in reader:
+                formatters[prop_name] = r['?formatterUrl']
+
+        json.dump(formatters, open(filepath, 'w'), indent=2, ensure_ascii=False)
+        return formatters
 
 @click.command()
 def get_wikidata_sample_links():
@@ -41,7 +51,7 @@ def get_wikidata_sample_links():
     BUCKET_SIZE = 100
     buckets = [set(entities[i*BUCKET_SIZE:(i+1)*BUCKET_SIZE]) for i in range(0, int((len(entities)/BUCKET_SIZE+1)))]
 
-    common.api_request_wikidata(query_info_for(buckets[0]))
+    print(get_url_formatters_for_properties())
 
 
 #TODO Code for api request
