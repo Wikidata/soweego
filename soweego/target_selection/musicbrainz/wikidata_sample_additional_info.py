7539b845268c519e7b37338fba935bf241a3cc85
Massimo Frasson <maxfrax@gmail.com>
2018-08-06 09:52:07 +0200

    Code refactor

--- soweego/target_selection/musicbrainz/wikidata_sample_additional_info.py
+++ soweego/target_selection/musicbrainz/wikidata_sample_additional_info.py
@@ -1,35 +1,33 @@
 import json
-from common import *
 import re
+from common import get_output_path
 
-path = get_output_path()
+PATH = get_output_path()
 
 def query_info_for(qids_bucket):
-    #TODO
-    #creating the big query with the optional chain
+    """Given a list of wikidata entities returns a query for getting some external ids"""
+
     properties = json.load(open('resources/properties_mapping.json'))
     query = "SELECT * WHERE{ VALUES ?id { %s } " % ' '.join(qids_bucket)
     for k, v in properties.items():
-        query += 'OPTIONAL { ?id wdt:%s ?%s . } ' % (v, re.sub('\W', '', k))
+        query += 'OPTIONAL { ?id wdt:%s ?%s . } ' % (v, re.sub(r'\W', '', k))
     query += "}"
-    print(query)
+    return query
     # do it foreach bucket
     #store everything in a file
 
 def query_wikipedia_articles_for(qids_bucket):
-    properties = json.load(open('resources/properties_mapping.json'))
+    """Given a list of wikidata entities returns a query for getting wikidata articles"""
+
     query = "SELECT * WHERE{ VALUES ?id { %s } " % ' '.join(qids_bucket)
     query += 'OPTIONAL { ?article schema:about ?id . }'
     query += "}"
-    print(query)
-
-
-#TODO
-#buckets of a hundred ids
-#call the function
+    return query
 
 labels_qid = json.load(open('../../wikidata/resources/musicians_sample_labels.json'))
 entities = ["wd:%s"%v for k,v in labels_qid.items()]
-bucket_size = 100
-buckets = [set(entities[i*bucket_size:(i+1)*bucket_size]) for i in range(0, int((len(entities)/bucket_size+1)))]
-query_wikipedia_articles_for(buckets[0])
+BUCKET_SIZE = 100
+buckets = [set(entities[i*BUCKET_SIZE:(i+1)*BUCKET_SIZE]) for i in range(0, int((len(entities)/BUCKET_SIZE+1)))]
+
+for bucket in buckets:
+    print(query_wikipedia_articles_for(bucket))
