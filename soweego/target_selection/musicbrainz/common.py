552ba0ba3ca9b32ebc360575e91c026b54ed692c
Massimo Frasson <maxfrax@gmail.com>
2018-08-06 11:45:25 +0200

    Improved handling of response in common api_request_wikidata

--- soweego/target_selection/musicbrainz/common.py
+++ soweego/target_selection/musicbrainz/common.py
@@ -16,5 +16,6 @@ def get_output_path():
     return output_dir_path
 
 def api_request_wikidata(query):
+    '''Given a wikidata sparql query returns the lines of the tsv response'''
     r = requests.get('https://query.wikidata.org/sparql', params={'query': query}, headers={'Accept': 'text/tab-separated-values'})
-    print(r.text)
+    return r.text.splitlines()
