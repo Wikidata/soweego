42e4c51226b0498ba67e2a7d518e5339c5f2d6d7
Marco Fossati <fossati@fbk.eu>
2018-08-13 18:11:52 +0200

    similar name strategy: cleaning and token-based matching. Closes #16

--- soweego/target_selection/common/matching_strategies.py
+++ soweego/target_selection/common/matching_strategies.py
@@ -10,12 +10,62 @@ __license__ = 'GPL-3.0'
 __copyright__ = 'Copyleft 2018, Hjfocs'
 
 import logging
+import re
 from collections import defaultdict
 from urllib.parse import urlsplit
 
 LOGGER = logging.getLogger(__name__)
+# URLs stopwords
 TOP_LEVEL_DOMAINS = set(['com', 'org', 'net', 'info', 'fm'])
 DOMAIN_PREFIXES = set(['www'])
+# Names processing
+NAMES_STOPWORDS = set(
+    ['sir', 'lord', 'mr', 'mrs', 'ms', 'miss', 'madam', 'jr', 'sr', 'phd',
+     'dr', 'mme', 'mlle', 'baron', 'baronet', 'bt', 'graf', 'gräfin', 'de',
+     'of', 'von', 'the']
+)
+ASCII_TRANSLATION_TABLE = str.maketrans({
+    'á': 'a', 'Á': 'A', 'à': 'a', 'À': 'A', 'ă': 'a', 'Ă': 'A', 'â': 'a',
+    'Â': 'A', 'å': 'a', 'Å': 'A', 'ã': 'a', 'Ã': 'A', 'ą': 'a', 'Ą': 'A',
+    'ā': 'a', 'Ā': 'A', 'ä': 'ae', 'Ä': 'AE', 'æ': 'ae', 'Æ': 'AE',
+    'ḃ': 'b', 'Ḃ': 'B', 'ć': 'c', 'Ć': 'C', 'ĉ': 'c', 'Ĉ': 'C', 'č': 'c',
+    'Č': 'C', 'ċ': 'c', 'Ċ': 'C', 'ç': 'c', 'Ç': 'C', 'ď': 'd', 'Ď': 'D',
+    'ḋ': 'd', 'Ḋ': 'D', 'đ': 'd', 'Đ': 'D', 'ð': 'dh', 'Ð': 'Dh',
+    'é': 'e', 'É': 'E', 'è': 'e', 'È': 'E', 'ĕ': 'e', 'Ĕ': 'E', 'ê': 'e',
+    'Ê': 'E', 'ě': 'e', 'Ě': 'E', 'ë': 'e', 'Ë': 'E', 'ė': 'e', 'Ė': 'E',
+    'ę': 'e', 'Ę': 'E', 'ē': 'e', 'Ē': 'E', 'ḟ': 'f', 'Ḟ': 'F', 'ƒ': 'f',
+    'Ƒ': 'F', 'ğ': 'g', 'Ğ': 'G', 'ĝ': 'g', 'Ĝ': 'G', 'ġ': 'g', 'Ġ': 'G',
+    'ģ': 'g', 'Ģ': 'G', 'ĥ': 'h', 'Ĥ': 'H', 'ħ': 'h', 'Ħ': 'H', 'í': 'i',
+    'Í': 'I', 'ì': 'i', 'Ì': 'I', 'î': 'i', 'Î': 'I', 'ï': 'i', 'Ï': 'I',
+    'ĩ': 'i', 'Ĩ': 'I', 'į': 'i', 'Į': 'I', 'ī': 'i', 'Ī': 'I', 'ĵ': 'j',
+    'Ĵ': 'J', 'ķ': 'k', 'Ķ': 'K', 'ĺ': 'l', 'Ĺ': 'L', 'ľ': 'l', 'Ľ': 'L',
+    'ļ': 'l', 'Ļ': 'L', 'ł': 'l', 'Ł': 'L', 'ṁ': 'm', 'Ṁ': 'M', 'ń': 'n',
+    'Ń': 'N', 'ň': 'n', 'Ň': 'N', 'ñ': 'n', 'Ñ': 'N', 'ņ': 'n', 'Ņ': 'N',
+    'ó': 'o', 'Ó': 'O', 'ò': 'o', 'Ò': 'O', 'ô': 'o', 'Ô': 'O', 'ő': 'o',
+    'Ő': 'O', 'õ': 'o', 'Õ': 'O', 'ø': 'oe', 'Ø': 'OE', 'ō': 'o', 'Ō': 'O',
+    'ơ': 'o', 'Ơ': 'O', 'ö': 'oe', 'Ö': 'OE', 'ṗ': 'p', 'Ṗ': 'P', 'ŕ': 'r',
+    'Ŕ': 'R', 'ř': 'r', 'Ř': 'R', 'ŗ': 'r', 'Ŗ': 'R', 'ś': 's', 'Ś': 'S',
+    'ŝ': 's', 'Ŝ': 'S', 'š': 's', 'Š': 'S', 'ṡ': 's', 'Ṡ': 'S', 'ş': 's',
+    'Ş': 'S', 'ș': 's', 'Ș': 'S', 'ß': 'SS', 'ť': 't', 'Ť': 'T', 'ṫ': 't',
+    'Ṫ': 'T', 'ţ': 't', 'Ţ': 'T', 'ț': 't', 'Ț': 'T', 'ŧ': 't', 'Ŧ': 'T',
+    'ú': 'u', 'Ú': 'U', 'ù': 'u', 'Ù': 'U', 'ŭ': 'u', 'Ŭ': 'U', 'û': 'u',
+    'Û': 'U', 'ů': 'u', 'Ů': 'U', 'ű': 'u', 'Ű': 'U', 'ũ': 'u', 'Ũ': 'U',
+    'ų': 'u', 'Ų': 'U', 'ū': 'u', 'Ū': 'U', 'ư': 'u', 'Ư': 'U', 'ü': 'ue',
+    'Ü': 'UE', 'ẃ': 'w', 'Ẃ': 'W', 'ẁ': 'w', 'Ẁ': 'W', 'ŵ': 'w', 'Ŵ': 'W',
+    'ẅ': 'w', 'Ẅ': 'W', 'ý': 'y', 'Ý': 'Y', 'ỳ': 'y', 'Ỳ': 'Y', 'ŷ': 'y',
+    'Ŷ': 'Y', 'ÿ': 'y', 'Ÿ': 'Y', 'ź': 'z', 'Ź': 'Z', 'ž': 'z', 'Ž': 'Z',
+    'ż': 'z', 'Ż': 'Z', 'þ': 'th', 'Þ': 'Th', 'µ': 'u', 'а': 'a', 'А': 'a',
+    'б': 'b', 'Б': 'b', 'в': 'v', 'В': 'v', 'г': 'g', 'Г': 'g', 'д': 'd',
+    'Д': 'd', 'е': 'e', 'Е': 'E', 'ё': 'e', 'Ё': 'E', 'ж': 'zh', 'Ж': 'zh',
+    'з': 'z', 'З': 'z', 'и': 'i', 'И': 'i', 'й': 'j', 'Й': 'j', 'к': 'k',
+    'К': 'k', 'л': 'l', 'Л': 'l', 'м': 'm', 'М': 'm', 'н': 'n', 'Н': 'n',
+    'о': 'o', 'О': 'o', 'п': 'p', 'П': 'p', 'р': 'r', 'Р': 'r', 'с': 's',
+    'С': 's', 'т': 't', 'Т': 't', 'у': 'u', 'У': 'u', 'ф': 'f', 'Ф': 'f',
+    'х': 'h', 'Х': 'h', 'ц': 'c', 'Ц': 'c', 'ч': 'ch', 'Ч': 'ch', 'ш': 'sh',
+    'Ш': 'sh', 'щ': 'sch', 'Щ': 'sch', 'ъ': '', 'Ъ': '', 'ы': 'y', 'Ы': 'y',
+    'ь': '', 'Ь': '', 'э': 'e', 'Э': 'e', 'ю': 'ju', 'Ю': 'ju', 'я': 'ja',
+    'Я': 'ja'
+})
 
 
 def perfect_string_match(datasets) -> dict:
@@ -40,11 +90,47 @@ def perfect_string_match(datasets) -> dict:
 
 def similar_link_match(source, target) -> dict:
     """Given 2 dictionaries `{link: identifier}`,
-    match similar links and return a dictionary `{id: id}`.
+    match similar links and return a dictionary `{source_id: target_id}`.
+
+    We treat links as natural language:
+    similarity means that a pair of links share a set of keywords.
 
     This strategy only applies to URLs.
     """
     return perfect_string_match((_process_links(source), _process_links(target)))
+
+
+def similar_name_match(source, target) -> dict:
+    """Given 2 dictionaries `{person_name: identifier}`,
+    match similar names and return a dictionary `{source_id: target_id}`.
+
+    This strategy only applies to people names.
+    """
+    return perfect_string_match((_process_names(source), _process_names(target)))
+
+
+def _process_names(dataset) -> dict:
+    """Convert a dictionary `{person_name: identifier}`
+    into a `{person_tokens: identifier}` one.
+
+    Name tokens are grouped by identifier and joined to treat them as a string.
+    """
+    tokenized = defaultdict(set)
+    processed = {}
+    for name, identifier in dataset.items():
+        LOGGER.debug('Identifier [%s]: processing name "%s"', identifier, name)
+        ascii_name = name.translate(ASCII_TRANSLATION_TABLE)
+        lowercased = ascii_name.lower()
+        split = re.split(r'\W+', lowercased)
+        LOGGER.debug('%s > %s > %s > %s', name, ascii_name, lowercased, split)
+        filtered = filter(lambda token: len(token) > 1, split)
+        for token in filtered:
+            if token and token not in NAMES_STOPWORDS:
+                tokenized[identifier].add(token)
+    for identifier, tokens in tokenized.items():
+        LOGGER.debug('Identifier [%s]: tokens = %s', identifier, tokens)
+        processed['|'.join(tokens)] = identifier
+    return processed
 
 
 def _process_links(dataset) -> dict:
@@ -65,7 +151,7 @@ def _process_links(dataset) -> dict:
 
 def _tokenize_url(url) -> set:
     """Tokenize a URL, removing stopwords.
-    Return `None` if the URL is invalid.
+        Return `None` if the URL is invalid.
     """
     split = None
     try:
