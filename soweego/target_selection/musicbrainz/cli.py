7c5bc1bacb5cf544b89316626ec858cd940b8829
Massimo Frasson <maxfrax@gmail.com>
2018-08-20 12:14:50 +0200

    labels_equal_strings_match refactor and fileautoformat

--- soweego/target_selection/musicbrainz/cli.py
+++ soweego/target_selection/musicbrainz/cli.py
@@ -1,9 +1,8 @@
 import click
-
 from soweego.target_selection.musicbrainz import musicbrainz_baseline_matcher
 
 CLI_COMMANDS = {
-    'baseline_matcher': musicbrainz_baseline_matcher.equal_strings_match,
+    'baseline_matcher': musicbrainz_baseline_matcher.labels_equal_strings_match,
     'get_users_urls': musicbrainz_baseline_matcher.get_users_urls,
     'links_match': musicbrainz_baseline_matcher.links_match
 }
@@ -14,4 +13,3 @@ CLI_COMMANDS = {
 def cli(ctx):
     """Operations over this target"""
     pass
-
--- soweego/target_selection/musicbrainz/musicbrainz_baseline_matcher.py
+++ soweego/target_selection/musicbrainz/musicbrainz_baseline_matcher.py
@@ -1,36 +1,43 @@
-#!/usr/bin/env python3
-# coding: utf-8
-
-import click
+import csv
 import json
 import os
-import csv
 import re
-from . import common
 from collections import defaultdict
+
+import click
 from soweego.target_selection.common import matching_strategies
 
-# ATTENTION remember to download the dump by running the bash script in the same folder of this script
-
 # Utilities functions
-def get_musicbrainz_artists_from_dump(opened_file_dump, label_column_index, id_column_index, total_number_columns):
+
+
+def get_musicbrainz_artists_from_dump(opened_file_dump,
+                                      label_column_index,
+                                      id_column_index,
+                                      total_number_columns):
     """Given an opened musicbrainz(mb) dump, return a dictionary label - mbid"""
+
     label_musicbrainz = {}
     fieldnames = [i for i in range(0, total_number_columns)]
 
-    musicbrainz_artists = csv.DictReader(opened_file_dump, dialect='excel-tab', fieldnames=fieldnames)
+    musicbrainz_artists = csv.DictReader(opened_file_dump,
+                                         dialect='excel-tab',
+                                         fieldnames=fieldnames)
 
     for row in musicbrainz_artists:
         # Checks if it's a person
         if row[10] == '1' or '\\N':
-            label_musicbrainz[row[label_column_index].lower()] = row[id_column_index]
+            lowered_label = row[label_column_index].lower()
+            label_musicbrainz[lowered_label] = row[id_column_index]
         else:
             print(row[10])
 
     return label_musicbrainz
 
+
 def get_label_musicbrainzid_dict():
-    """Returns the name-musicbrainzid dictionary. If it's not stored, creates a brand new file"""
+    """Returns the name-musicbrainzid dictionary.
+    If it's not stored, creates a brand new file"""
+
     filepath = '%s/artists.json' % common.get_output_path()
     if os.path.isfile(filepath):
         return json.load(open(filepath))
@@ -38,35 +45,49 @@ def get_label_musicbrainzid_dict():
         artists = {}
         with open('%s/musicbrainz_dump_20180725-001823/mbdump/artist' % common.get_output_path()) as tsvfile:
             artists = get_musicbrainz_artists_from_dump(tsvfile, 2, 0, 20)
-            artists.update(get_musicbrainz_artists_from_dump(tsvfile, 3, 0, 20))
+            artists.update(
+                get_musicbrainz_artists_from_dump(tsvfile, 3, 0, 20))
 
         with open('%s/musicbrainz_dump_20180725-001823/mbdump/artist_alias' % common.get_output_path()) as tsvfile:
-            artists.update(get_musicbrainz_artists_from_dump(tsvfile, 2, 1, 16))
-            artists.update(get_musicbrainz_artists_from_dump(tsvfile, 7, 1, 16))
+            artists.update(
+                get_musicbrainz_artists_from_dump(tsvfile, 2, 1, 16))
+            artists.update(
+                get_musicbrainz_artists_from_dump(tsvfile, 7, 1, 16))
 
         json.dump(artists, open(filepath, 'w'), indent=2, ensure_ascii=False)
         return artists
 
+
 @click.command()
-def equal_strings_match():
+@click.argument('labels_qid_sample', type=click.Path(exists=True))
+@click.option('--output', '-o', default='output', type=click.Path(exists=True))
+def labels_equal_strings_match(labels_qid_sample, output):
     """Creates the equal strings match output file"""
     # Wikidata sample loading
-    labels_qid = json.load(open('musicians_wikidata_sample.json'))
-    matches = matching_strategies.perfect_string_match((labels_qid, get_label_musicbrainzid_dict()))
-    json.dump(matches, open('%s/equal_strings_matches.json' % common.get_output_path(), 'w'), indent=2, ensure_ascii=False)
+    labels_qid = json.load(open(labels_qid_sample))
+    matches = matching_strategies.perfect_string_match(
+        (labels_qid, get_label_musicbrainzid_dict()))
+    output_full_path = os.path.join(output, 'labels_equal_strings_match.json')
+    json.dump(matches, open(output_full_path, 'w'),
+              indent=2, ensure_ascii=False)
+
 
 def get_url_domains():
     """Finds all the domains to which the artists are connected"""
     fieldnames = [i for i in range(0, 5)]
-    domain_regex = '^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/?\n]+)'
+    domain_regex = r'^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/?\n]+)'
     domains = defaultdict(int)
     with open('%s/musicbrainz_dump_20180725-001823/mbdump/url' % common.get_output_path(), "r") as tsvfile:
-        urls = csv.DictReader(tsvfile, dialect='excel-tab', fieldnames=fieldnames)
+        urls = csv.DictReader(
+            tsvfile, dialect='excel-tab', fieldnames=fieldnames)
         for url in urls:
             domain = re.search(domain_regex, url[2]).group(1)
             domains[domain] += 1
-    towrite = {domain:count for (domain, count) in domains.items() if count > 10000}
-    json.dump(towrite, open('%s/urls.json' % common.get_output_path(), 'w'), indent=2, ensure_ascii=False)
+    towrite = {domain: count for (domain, count)
+               in domains.items() if count > 10000}
+    json.dump(towrite, open('%s/urls.json' %
+                            common.get_output_path(), 'w'), indent=2, ensure_ascii=False)
+
 
 def _get_users_urls(dump_folder_path, output):
     output_full_path = os.path.join(output, 'url_artist.json')
@@ -78,36 +99,41 @@ def _get_users_urls(dump_folder_path, output):
     else:
         with open(os.path.join(dump_folder_path, 'mbdump/l_artist_url'), "r") as tsvfile:
             fieldnames = [i for i in range(0, 6)]
-            url_relationships = csv.DictReader(tsvfile, dialect='excel-tab', fieldnames=fieldnames)
+            url_relationships = csv.DictReader(
+                tsvfile, dialect='excel-tab', fieldnames=fieldnames)
             for relationship in url_relationships:
                 # url id matched with its user id
                 urlid_id[relationship[3]] = relationship[2]
 
         with open(os.path.join(dump_folder_path, 'mbdump/url'), "r") as tsvfile:
             fieldnames = [i for i in range(0, 5)]
-            urls = csv.DictReader(tsvfile, dialect='excel-tab', fieldnames=fieldnames)
+            urls = csv.DictReader(
+                tsvfile, dialect='excel-tab', fieldnames=fieldnames)
             for url in urls:
                 # Translates the url ids stored before in the respective urls
                 if url[0] in urlid_id:
                     url_id[url[2]] = urlid_id[url[0]]
 
-        json.dump(url_id, open(output_full_path, 'w'), indent=2, ensure_ascii=False)
+        json.dump(url_id, open(output_full_path, 'w'),
+                  indent=2, ensure_ascii=False)
         return url_id
+
 
 @click.command()
 @click.argument('dump_folder_path', type=click.Path(exists=True))
-@click.option('--output', '-o', default=common.get_output_path(), type=click.Path(exists=True))
+@click.option('--output', '-o', default='output', type=click.Path(exists=True))
 def get_users_urls(dump_folder_path, output):
     """Creates the json containing url - artist id"""
     _get_users_urls(dump_folder_path, output)
+
 
 @click.command()
 @click.argument('dump_folder_path', type=click.Path(exists=True))
 @click.argument('links_qid_dictionary', type=click.Path(exists=True))
 @click.argument('sitelinks_qid_dictionary', type=click.Path(exists=True))
-@click.option('--output', '-o', default=common.get_output_path(), type=click.Path(exists=True))
+@click.option('--output', '-o', default='output', type=click.Path(exists=True))
 def links_match(dump_folder_path, links_qid_dictionary, sitelinks_qid_dictionary, output):
-    # Loads url-musicbrainz id 
+    # Loads url-musicbrainz id
     url_mbid = _get_users_urls(dump_folder_path, output)
     # Loads link - wikidata id
     link_qid = json.load(open(links_qid_dictionary))
@@ -116,7 +142,9 @@ def links_match(dump_folder_path, links_qid_dictionary, sitelinks_qid_dictionary
     # Equal strigs match among urls
     link_qid.update(sitelink_qid)
 
-    ids_matching = matching_strategies.perfect_string_match((url_mbid, link_qid))
+    ids_matching = matching_strategies.perfect_string_match(
+        (url_mbid, link_qid))
 
     full_outputh_path = os.path.join(output, 'link_match.json')
-    json.dump(ids_matching, open(full_outputh_path, 'w'), indent=2, ensure_ascii=False)
+    json.dump(ids_matching, open(full_outputh_path, 'w'),
+              indent=2, ensure_ascii=False)
