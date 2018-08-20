613bd6b8bcbd3a0ba6389ed4a9a29bd4321c678b
Massimo Frasson <maxfrax@gmail.com>
2018-08-20 15:25:12 +0200

    Cli for common matching strategies

--- soweego/target_selection/cli.py
+++ soweego/target_selection/cli.py
@@ -1,15 +1,15 @@
 import click
-
-from soweego.target_selection import musicbrainz, discogs
+from soweego.target_selection import common, discogs, musicbrainz
 
 CLI_COMMANDS = {
     'musicbrainz': musicbrainz.cli.cli,
     'discogs': discogs.cli.cli,
+    'common': common.cli.cli
 }
+
 
 @click.group(commands=CLI_COMMANDS)
 @click.pass_context
 def cli(ctx):
     """Investigation on candidate targets"""
     pass
-
