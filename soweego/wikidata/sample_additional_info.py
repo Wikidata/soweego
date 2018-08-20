301c670a8486e2a656e1863214ab68b1d53e0603
Massimo Frasson <maxfrax@gmail.com>
2018-08-20 11:20:15 +0200

    get_birth_death_dates_for_sample command

--- soweego/wikidata/sample_additional_info.py
+++ soweego/wikidata/sample_additional_info.py
@@ -2,6 +2,7 @@ import json
 import csv
 import os
 import re
+import iso8601
 from collections import defaultdict
 import click
 from ..target_selection.musicbrainz import common
@@ -28,13 +29,14 @@ def query_wikipedia_articles_for(qids_bucket):
 def query_birth_death(qids_bucket):
     """Given a list of wikidata entities returns a query for getting their birth and death dates"""
 
-    query = "SELECT ?birth ?b_precision ?death ?d_precision WHERE{ VALUES ?id { %s } " % ' '.join(qids_bucket)
+    query = "SELECT ?id ?birth ?b_precision ?death ?d_precision WHERE{ VALUES ?id { %s } " % ' '.join(qids_bucket)
     query += '?id p:P569 ?b. ?b psv:P569 ?t1 . ?t1 wikibase:timePrecision ?b_precision . ?t1 wikibase:timeValue ?birth . OPTIONAL { ?id p:P570 ?d . ?d psv:P570 ?t2 . ?t2 wikibase:timePrecision ?d_precision . ?t2 wikibase:timeValue ?death . }'
     query += "}"
 
     return query
 
 #JSONs creation
+#TODO convert to command using click
 
 def get_url_formatters_for_properties(properties):
     """Retrieves the url formatters for the properties listed in the given dict"""
@@ -68,6 +70,22 @@ def get_sample_buckets(sample_path):
     labels_qid = json.load(open(sample_path))
     entities = ["wd:%s"%v for k,v in labels_qid.items()]
     return [set(entities[i*size:(i+1)*size]) for i in range(0, int((len(entities)/size+1)))]
+
+def get_date_strings(timestamp, precision):
+    """Given a timestamp and a wikidata date precision, returns a combination of strings"""
+    if timestamp:
+        timesplit = timestamp.split('^')[0]
+        precisionsplit = precision.split('^')[0]
+        date = iso8601.parse_date(timesplit).date()
+
+        if precisionsplit == "11":
+            return [str(date.year), '%s%s' % (date.year, date.month), '%s%s%s' % (date.year, date.month, date.day)]
+        elif precisionsplit == "10":
+            return [str(date.year), '%s%s' % (date.year, date.month)]
+        else:
+            return [str(date.year)]
+    else:
+        return []
 
 @click.command()
 @click.argument('sample_path', type=click.Path(exists=True))
@@ -128,3 +146,33 @@ def get_sitelinks_for_sample(sample_path, output):
             url_id[site_url] = entity_id
 
     json.dump(url_id, open(filepath, 'w'), indent=2, ensure_ascii=False)
+
+@click.command()
+@click.argument('sample_path', type=click.Path(exists=True))
+@click.option('--output', '-o', default='output', type=click.Path(exists=True))
+def get_birth_death_dates_for_sample(sample_path, output):
+    # Creates buckets for artist from the sample. Technique to fix quering issues
+    qid_labels = {v: k for k, v in json.load(open(sample_path, 'r')).items()}
+    buckets = get_sample_buckets(sample_path)
+
+    labeldate_qid = {}
+    filepath = os.path.join(output, 'sample_dates.json')
+
+    for bucket in buckets:
+        response = common.api_request_wikidata(query_birth_death(bucket))
+        dates_collection = csv.DictReader(response, dialect='excel-tab')
+        for date_row in dates_collection:
+            qid = get_wikidata_id_from_uri(date_row['?id'])
+            # creates the combination of all birth dates strings and all death dates strings
+            if date_row['?birth']:
+                for b in get_date_strings(date_row['?birth'], date_row['?b_precision']):
+                    if date_row['?death']:
+                        for d in get_date_strings(date_row['?death'], date_row['?d_precision']):
+                            labeldate_qid['%s|%s-%s' % (qid_labels[qid], b, d)] = qid
+                    else:
+                        labeldate_qid['%s|%s' % (qid_labels[qid], b)] = qid
+            else:
+                for d in get_date_strings(date_row['?death'], date_row['?d_precision']):
+                    labeldate_qid['%s|-%s' % (qid_labels[qid], d)] = qid
+
+    json.dump(labeldate_qid, open(filepath, 'w'), indent=2, ensure_ascii=False)
