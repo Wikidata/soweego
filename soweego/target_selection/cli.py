c7349ee22265b63f1a29e626b46c26867495da81
Marco Fossati <fossati@fbk.eu>
2018-08-20 18:13:20 +0200

    comply with PEP8 and name CLI groups

--- soweego/target_selection/cli.py
+++ soweego/target_selection/cli.py
@@ -1,15 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""TODO module docstring"""
+
+__author__ = 'Marco Fossati'
+__email__ = 'fossati@spaziodati.eu'
+__version__ = '1.0'
+__license__ = 'GPL-3.0'
+__copyright__ = 'Copyleft 2018, Hjfocs'
+
 import click
 
-from soweego.target_selection import musicbrainz, discogs
+from soweego.target_selection import discogs, musicbrainz
 
 CLI_COMMANDS = {
     'musicbrainz': musicbrainz.cli.cli,
     'discogs': discogs.cli.cli,
 }
 
-@click.group(commands=CLI_COMMANDS)
+
+@click.group(name='target_selection', commands=CLI_COMMANDS)
 @click.pass_context
 def cli(ctx):
-    """Investigation on candidate targets"""
+    """Investigation on candidate targets."""
     pass
-
--- soweego/wikidata/cli.py
+++ soweego/wikidata/cli.py
@@ -1,7 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""TODO module docstring"""
+
+__author__ = 'Marco Fossati'
+__email__ = 'fossati@spaziodati.eu'
+__version__ = '1.0'
+__license__ = 'GPL-3.0'
+__copyright__ = 'Copyleft 2018, Hjfocs'
+
+
 import click
 
-from soweego.wikidata import query_on_values
-from soweego.wikidata import sample_additional_info
+from soweego.wikidata import query_on_values, sample_additional_info
 
 CLI_COMMANDS = {
     'query_on_values': query_on_values.main,
@@ -13,6 +24,5 @@ CLI_COMMANDS = {
 @click.group(name='wikidata', commands=CLI_COMMANDS)
 @click.pass_context
 def cli(ctx):
-    """Read/write operations on the knowledge base"""
+    """Read/write operations on Wikidata."""
     pass
-
