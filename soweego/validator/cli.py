75dafd558f5a5f62701a09b6738444ab42a12923
Marco Fossati <fossati@fbk.eu>
2018-08-27 18:08:39 +0200

    check existence of links: first working implementation. Closes #41

---
+++ soweego/validator/cli.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""TODO module docstring"""
+
+__author__ = 'Marco Fossati'
+__email__ = 'fossati@spaziodati.eu'
+__version__ = '1.0'
+__license__ = 'GPL-3.0'
+__copyright__ = 'Copyleft 2018, Hjfocs'
+
+import click
+
+from soweego.validator import checks
+
+CLI_COMMANDS = {
+    'check_existence': checks.check_existence
+}
+
+
+@click.group(name='validator', commands=CLI_COMMANDS)
+@click.pass_context
+def cli(ctx):
+    """Sanity checks of existing identifiers in Wikidata."""
+    pass
