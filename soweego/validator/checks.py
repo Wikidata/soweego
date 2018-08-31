e1045b19b098e6dd8e9411d31771a1e3173d0aaa
Massimo Frasson <maxfrax@gmail.com>
2018-08-31 14:56:15 +0200

    Fixed syntax issues due to adoption of generators

--- soweego/validator/checks.py
+++ soweego/validator/checks.py
@@ -15,7 +15,6 @@ from collections import defaultdict
 from csv import DictReader
 
 import click
-
 from soweego.wikidata.sparql_queries import (instance_based_identifier_query,
                                              occupation_based_identifier_query)
 
@@ -26,8 +25,9 @@ LOGGER = logging.getLogger(__name__)
 @click.argument('wikidata_query_type', type=click.Choice(['instance', 'occupation']))
 @click.argument('class_qid')
 @click.argument('catalog_pid')
+@click.argument('target_identifiers', type=click.File())
 @click.option('-o', '--outdir', type=click.Path(), default='output', help="default: 'output'")
-def check_existence(wikidata_query_type, class_qid, catalog_pid, outdir):
+def check_existence(wikidata_query_type, class_qid, catalog_pid, target_identifiers, outdir):
     """Check the existence of identifier statements.
 
     Dump a JSON file of nonexistent ones ``{identifier: QID}``
@@ -37,15 +37,14 @@ def check_existence(wikidata_query_type, class_qid, catalog_pid, outdir):
         query_function = instance_based_identifier_query
     elif wikidata_query_type == 'occupation':
         query_function = occupation_based_identifier_query
-    # TODO fill the appropriate params in query_function
-    # e.g., query_result = query_function(...)
+
     qids_to_ids = {}
     invalid = defaultdict(list)
-    fields = query_result.fieldnames
-    for row in query_result:
-        qids_to_ids[row[fields[1]]] = row[fields[0]]
-    nonexistent_identifiers = set(
-        qids_to_ids.keys()).difference(set(i.rstrip() for i in target_identifiers))
+    for row in query_function(class_qid, catalog_pid, 1000):
+        qids_to_ids.update(row)
+
+    nonexistent_identifiers = set(qids_to_ids.keys()).difference(
+        set(i.rstrip() for i in target_identifiers))
     for identifier in nonexistent_identifiers:
         invalid[identifier].append(qids_to_ids[identifier])
     json.dump(invalid, outdir, indent=2)
