1d7d71a1a52f0c4f02bcbc950a3b61f9a5bf30a3
Marco Fossati <fossati@fbk.eu>
2018-08-31 16:06:06 +0200

    actual base schema

--- soweego/importer/models/orm/base_entity.py
+++ soweego/importer/models/orm/base_entity.py
@@ -1,28 +1,45 @@
-from sqlalchemy import Column
-from sqlalchemy import Index
-from sqlalchemy import Integer
-from sqlalchemy import String
-from sqlalchemy import Date
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Edoardo Lenzi'
+__email__ = 'edoardolenzi9@gmail.com'
+__version__ = '1.0'
+__license__ = 'GPL-3.0'
+__copyright__ = 'Copyleft 2018, lenzi.edoardo'
+
+from sqlalchemy import Column, Date, Index, Integer, String
 from sqlalchemy.engine import Engine
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class BaseEntity(Base):
     __tablename__ = "base_entity"
-    id = Column(Integer, unique=True, primary_key=True, autoincrement=True)
+    internal_id = Column(Integer(11), unique=True,
+                         primary_key=True, autoincrement=True)
 
-    key = Column(String, unique=True) # external, uri key
-    name = Column(String) # person name (<name> <surname>)
-    birth_date = Column(Date) # date of birth (express date precision)
-    death_date = Column(Date) # date of death (express date precision)
-    link = Column(String) # link of a related resource
-    link_domain = Column(String) # domain of the link
-
-    Index("name_index", name)
+    # Catalog identifier, indexed
+    catalog_id = Column(String(32), nullable=False, index=True)
+    # Full name (<name> <surname>)
+    name = Column(String, nullable=False)
+    # Date of birth
+    born = Column(Date)
+    # Date of birth precision
+    born_precision = Column(Integer(2))
+    # Date of death
+    died = Column(Date)
+    # Date of death precision
+    died_precision = Column(Integer(2))
+    # Wikimedia sister project link
+    site_link = Column(String)
+    # Third-party link (should come from a whitelist)
+    link = Column(String)
+    # Full-text index over the 'name' column
+    Index('name_index', name, mysql_prefix='FULLTEXT')
 
     def __repr__(self) -> str:
-        return "<BaseEntity(key='{0}', name='{1}')>".format(self.key, self.name)
+        return "<BaseEntity(catalog_id='{0}', name='{1}')>".format(self.catalog_id, self.name)
 
     def drop(self, engine: Engine) -> None:
         self.__table__.drop(engine)
