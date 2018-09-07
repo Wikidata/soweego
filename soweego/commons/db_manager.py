81a2a9820bedcf4b95f9405307afbff8532ab6e0
Massimo Frasson <maxfrax@gmail.com>
2018-09-07 10:08:42 +0200

    Removed echo from db connection to speed up the import

--- soweego/commons/db_manager.py
+++ soweego/commons/db_manager.py
@@ -9,27 +9,25 @@ __version__ = '1.0'
 __license__ = 'GPL-3.0'
 __copyright__ = 'Copyleft 2018, lenzi.edoardo'
 
-from sqlalchemy.engine import Engine
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
 import logging
 
+from soweego.commons import constants as const
 from soweego.commons import json_utils
 from soweego.commons import localizations as loc
-from soweego.commons import constants as const
-
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 DECLARATIVE_BASE = declarative_base()
 LOGGER = logging.getLogger(__name__)
 
 
 class DBManager(object):
-    
-    """Class that exposes some primiteves for the DB access"""
- 
-    __engine: object
 
+    """Class that exposes some primiteves for the DB access"""
+
+    __engine: object
 
     def __init__(self, credentials_path='TODO default credentials path'):
         try:
@@ -38,23 +36,22 @@ class DBManager(object):
             LOGGER.warning(loc.MISSING_CREDENTIALS)
             raise Exception(loc.MISSING_CREDENTIALS)
 
-        db_engine = credentials[const.DB_ENGINE_KEY] 
+        db_engine = credentials[const.DB_ENGINE_KEY]
         db_name = credentials[const.PROD_DB_KEY]
         user = credentials[const.USER_KEY]
         password = credentials[const.PASSWORD_KEY]
         host = credentials[const.HOST_KEY]
         try:
             self.__engine = create_engine(
-                '{0}://{1}:{2}@{3}/{4}'.format(db_engine, user, password, host, db_name), 
-                echo=True)
-        except:  
+                '{0}://{1}:{2}@{3}/{4}'.format(db_engine,
+                                               user, password, host, db_name),
+                echo=False)
+        except:
             LOGGER.warning(loc.FAIL_CREATE_ENGINE)
-
 
     def get_engine(self) -> Engine:
         """returns the current SqlAlchemy-Engine instance"""
         return self.__engine
-
 
     def new_session(self) -> object:
         """Creates a new DB session"""
@@ -62,14 +59,12 @@ class DBManager(object):
         session = Session()
         return session
 
-
     def create(self, table) -> None:
         """Creates the schema of the table (table can be an instance or a type)"""
-        DECLARATIVE_BASE.metadata.create_all(self.__engine, 
+        DECLARATIVE_BASE.metadata.create_all(self.__engine,
                                              tables=[table.__table__])
-
 
     def drop(self, table) -> None:
         """Drops the table (table can be an instance or a type)"""
-        DECLARATIVE_BASE.metadata.drop_all(self.__engine, 
+        DECLARATIVE_BASE.metadata.drop_all(self.__engine,
                                            tables=[table.__table__])
