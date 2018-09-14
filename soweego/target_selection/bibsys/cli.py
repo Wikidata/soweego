cb73f27f8dd8fe7b38a3d58f1330cedcef8fa983
Marco Fossati <fossati@fbk.eu>
2018-09-14 17:33:18 +0200

    CLI command for dump extraction

--- soweego/target_selection/bibsys/cli.py
+++ soweego/target_selection/bibsys/cli.py
@@ -3,8 +3,10 @@ import click
 from soweego.target_selection.bibsys import bibsys_baseline_matcher
 
 CLI_COMMANDS = {
+    'extract_data': bibsys_baseline_matcher.download_and_scrape,
     'baseline_matcher': bibsys_baseline_matcher.equal_strings_match
 }
+
 
 @click.group(name='bibsys', commands=CLI_COMMANDS)
 @click.pass_context
