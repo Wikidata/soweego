2b48afc13ca59bd59a574e837a89fdfbe5c07595
Marco Fossati <fossati@fbk.eu>
2018-09-21 18:52:32 +0200

    Wikidata terms for IMDb

--- soweego/wikidata/vocabulary.py
+++ soweego/wikidata/vocabulary.py
@@ -19,26 +19,26 @@ STATED_IN_PID = 'P248'
 RETRIEVED_PID = 'P813'
 
 # Target catalog items
-BIBSYS_QID = 'Q4584301'
 DISCOGS_QID = 'Q504063'
+IMDB_QID = 'Q37312'
 MUSICBRAINZ_QID = 'Q14005'
 TWITTER_QID = 'Q918'
 
 # Identifier properties
-BIBSYS_PID = 'P1015'
 DISCOGS_ARTIST_PID = 'P1953'
+IMDB_PID = 'P345'
 MUSICBRAINZ_ARTIST_PID = 'P434'
 TWITTER_USERNAME_PID = 'P2002'
 
 # Target catalogs helper dictionary
 CATALOG_MAPPING = {
-    'bibsys': {
-        'qid': BIBSYS_QID,
-        'pid': BIBSYS_PID
-    },
     'discogs': {
         'qid': DISCOGS_QID,
         'pid': DISCOGS_ARTIST_PID
+    },
+    'imdb': {
+        'qid': IMDB_QID,
+        'pid': IMDB_PID
     },
     'musicbrainz': {
         'qid': MUSICBRAINZ_QID,
