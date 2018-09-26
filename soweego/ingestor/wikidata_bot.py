2cd9139430680360d51b88910e3dfca9466305d5
Marco Fossati <fossati@fbk.eu>
2018-09-26 17:59:06 +0200

    bot deprecate invalid identifier statements

--- soweego/ingestor/wikidata_bot.py
+++ soweego/ingestor/wikidata_bot.py
@@ -61,7 +61,21 @@ def delete_identifiers_cli(catalog_name, invalid_identifiers, sandbox):
     """
     if sandbox:
         LOGGER.info('Running on the Wikidata sandbox item')
-    delete_identifiers(json.load(invalid_identifiers), catalog_name, sandbox)
+    delete_or_deprecate_identifiers('delete', json.load(
+        invalid_identifiers), catalog_name, sandbox)
+
+
+@click.command()
+@click.argument('catalog_name', type=click.Choice(['discogs', 'imdb', 'musicbrainz', 'twitter']))
+@click.argument('invalid_identifiers', type=click.File())
+@click.option('-s', '--sandbox', is_flag=True, help='Perform all edits in a random Wikidata sandbox item')
+def deprecate_identifiers_cli(catalog_name, invalid_identifiers, sandbox):
+    """Bot deprecate invalid identifiers from existing Wikidata items.
+    """
+    if sandbox:
+        LOGGER.info('Running on the Wikidata sandbox item')
+    delete_or_deprecate_identifiers('deprecate', json.load(
+        invalid_identifiers), catalog_name, sandbox)
 
 
 def add_identifiers(matches: dict, catalog_name: str, sandbox: bool) -> None:
@@ -78,26 +92,23 @@ def add_identifiers(matches: dict, catalog_name: str, sandbox: bool) -> None:
         LOGGER.info('Processing %s match: %s -> %s',
                     catalog_name, qid, catalog_id)
         if sandbox:
-            _add_or_delete('add', vocabulary.SANDBOX_1_QID,
-                           catalog_id, catalog_name)
+            _add(vocabulary.SANDBOX_1_QID, catalog_id, catalog_name)
         else:
-            _add_or_delete('add', qid, catalog_id, catalog_name)
+            _add(qid, catalog_id, catalog_name)
 
 
-def _add_or_delete(action, qid, catalog_id, catalog_name):
-    actions = {
-        'add': _add_identifier,
-        'delete': _delete_identifier
-    }
-    actions[action](qid, catalog_id, catalog_name)
+def delete_or_deprecate_identifiers(action: str, invalid: dict, catalog_name: str, sandbox: bool) -> None:
+    """Delete or deprecate invalid identifier statements from existing Wikidata items.
 
-
-def delete_identifiers(invalid: dict, catalog_name: str, sandbox: bool) -> None:
-    """Delete invalid identifier statements from existing Wikidata items.
-
-    Identifiers that should be deleted come from the first validation check
+    Deletion candidates come from the validation criterion 1
     as per :func:`soweego.validator.checks.check_existence`.
 
+    Deprecation candidates come from validation criteria 2 or 3
+    as per :func:`soweego.validator.checks.check_links` and
+    :func:`soweego.validator.checks.check_metadata`.
+
+    :param action: either ``delete`` or ``deprecate``
+    :type action: str 
     :param invalid: a ``{invalid_catalog_identifier: [list of QIDs]}`` dictionary
     :type invalid: dict
     :param catalog_name: the name of the target catalog, e.g., ``discogs``
@@ -107,16 +118,16 @@ def delete_identifiers(invalid: dict, catalog_name: str, sandbox: bool) -> None:
     """
     for catalog_id, qids in invalid.items():
         for qid in qids:
-            LOGGER.info('Deleting %s identifier: %s -> %s',
-                        catalog_name, qid, catalog_id)
+            LOGGER.info('Will %s %s identifier: %s -> %s',
+                        action, catalog_name, qid, catalog_id)
             if sandbox:
-                _add_or_delete('delete', vocabulary.SANDBOX_1_QID,
-                               catalog_id, catalog_name)
+                _delete_or_deprecate(action, vocabulary.SANDBOX_1_QID,
+                                     catalog_id, catalog_name)
             else:
-                _add_or_delete('delete', qid, catalog_id, catalog_name)
+                _delete_or_deprecate(action, qid, catalog_id, catalog_name)
 
 
-def _add_identifier(qid: str, catalog_id: str, catalog_name: str) -> None:
+def _add(qid: str, catalog_id: str, catalog_name: str) -> None:
     subject = pywikibot.ItemPage(REPO, qid)
     catalog_terms = vocabulary.CATALOG_MAPPING.get(catalog_name)
     claim = pywikibot.Claim(REPO, catalog_terms['pid'])
@@ -131,7 +142,7 @@ def _add_identifier(qid: str, catalog_id: str, catalog_name: str) -> None:
     LOGGER.info('Added %s identifier statement to %s', catalog_name, qid)
 
 
-def _delete_identifier(qid: str, catalog_id: str, catalog_name: str) -> None:
+def _delete_or_deprecate(action: str, qid: str, catalog_id: str, catalog_name: str) -> None:
     item = pywikibot.ItemPage(REPO, qid)
     catalog_terms = vocabulary.CATALOG_MAPPING.get(catalog_name)
     item_data = item.get()
@@ -140,18 +151,23 @@ def _delete_identifier(qid: str, catalog_id: str, catalog_name: str) -> None:
     # the input item is supposed to have at least an identifier claim.
     # We never know, Wikidata is live.
     if not item_claims:
-        LOGGER.error('%s has no claims. Cannot delete %s identifier %s',
-                     qid, catalog_name, catalog_id)
+        LOGGER.error('%s has no claims. Cannot %s %s identifier %s',
+                     qid, action, catalog_name, catalog_id)
         return
     catalog_pid = catalog_terms['pid']
     identifier_claims = item_claims.get(catalog_pid)
     # Same comment as the previous one
     if not identifier_claims:
-        LOGGER.error('%s has no %s claims. Cannot delete %s identifier %s',
-                     qid, catalog_pid, catalog_name, catalog_id)
+        LOGGER.error('%s has no %s claims. Cannot %s %s identifier %s',
+                     qid, action, catalog_pid, catalog_name, catalog_id)
         return
     for claim in identifier_claims:
         if claim.getTarget() == catalog_id:
-            item.removeClaims([claim], summary='Invalid identifier')
-            LOGGER.debug('Deleted claim: %s', claim.toJSON())
-    LOGGER.info('Deleted %s identifier statement from %s', catalog_name, qid)
+            if action == 'delete':
+                item.removeClaims([claim], summary='Invalid identifier')
+            elif action == 'deprecate':
+                claim.changeRank(
+                    'deprecated', summary='Deprecate arguable claim')
+            LOGGER.debug('%s claim: %s', action.title() + 'd', claim.toJSON())
+    LOGGER.info('%s %s identifier statement from %s',
+                action.title() + 'd', catalog_name, qid)
