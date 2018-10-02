aa83617e241a3f5d212dc9a4d10812101b3261b4
Massimo Frasson <maxfrax@gmail.com>
2018-10-02 16:10:54 +0200

    Fixed env variable declaration

--- scripts/docker/run_soweego_test.sh
+++ scripts/docker/run_soweego_test.sh
@@ -1,7 +1,4 @@
 #! /bin/bash
 
-# Put here the location of your dump work dir
-export DUMP_FOLDER="/Volumes/Dati/soweego_files"
-
 docker-compose -f "docker-compose.dev.yml" up -d --build
 docker-compose -f "docker-compose.dev.yml" exec soweego /bin/bash
