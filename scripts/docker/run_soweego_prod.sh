4d3e5a183d325294792ff63e16e6fa1c54644937
Massimo Frasson <maxfrax@gmail.com>
2018-10-04 10:37:16 +0200

    Improved production script

--- scripts/docker/run_soweego_prod.sh
+++ scripts/docker/run_soweego_prod.sh
@@ -1,3 +1,25 @@
 #! /bin/bash
 
-docker run -it --name soweego  --env-file .env maxfrax/soweego:latest /bin/bash
+old_credentials="soweego/importer/resources/db_credentials.json.old"
+
+# Checks if the arguments are given
+if [ -z "$1" ]
+  then
+    echo "Running soweego with test database credentials"
+    if [ -f "$old_credentials" ]
+    then
+        mv soweego/importer/resources/db_credentials.json.old soweego/importer/resources/db_credentials.json
+    fi
+    docker build --rm -f "Dockerfile" -t maxfrax/soweego:latest .
+    docker run -it --rm --name soweego  --env-file .env maxfrax/soweego:latest /bin/bash
+    exit 0
+fi
+
+echo "Running soweego with given database credentials"
+if [ ! -f "$old_credentials" ]
+then
+    mv soweego/importer/resources/db_credentials.json $old_credentials
+fi
+cp $1 soweego/importer/resources/db_credentials.json
+docker build --rm -f "Dockerfile" -t maxfrax/soweego:latest .
+docker run -it --rm --name soweego  --env-file .env maxfrax/soweego:latest /bin/bash
