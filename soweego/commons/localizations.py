38a5d6d179e5319f432e39f25e69e88238946a73
Marco Fossati <fossati@fbk.eu>
2018-10-31 18:11:58 +0100

    don't silently eat all exceptions; drop & create a list of tables

--- soweego/commons/localizations.py
+++ soweego/commons/localizations.py
@@ -5,7 +5,7 @@
 
 # Exceptions
 MISSING_CREDENTIALS = 'DB Credentials or Configs not found'
-FAIL_CREATE_ENGINE = 'ORM fails to create the DB engine, checks your credentials'
+FAIL_CREATE_ENGINE = 'Failed to create the DB engine, please check your credentials. Reason: %s'
 FAIL_DOWNLOAD = 'Fails on dump download'
 FAIL_HANDLER = 'Handler fails on dump scraping'
 MALFORMED_ROW = 'Malformed Row, brokes the structure <subject> <predicate> <object>'
