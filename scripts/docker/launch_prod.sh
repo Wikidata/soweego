24f18288988e8efb1af209d7a8a70243e3fce191
Massimo Frasson <maxfrax@gmail.com>
2018-11-26 13:17:58 +0100

    Closes #131

--- scripts/docker/launch_prod.sh
+++ scripts/docker/launch_prod.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 
-old_credentials="soweego/importer/resources/db_credentials.json.old"
-
 source .env
 
-# Checks if the arguments are given
-if [ -z "$1" ]
-  then
-    echo "Running soweego with test database credentials"
-    if [ -f "$old_credentials" ]
-    then
-        mv soweego/importer/resources/db_credentials.json.old soweego/importer/resources/db_credentials.json
-    fi
-    docker build --rm -f "Dockerfile.prod" -t maxfrax/soweego:latest .
-    docker run -it --rm --name soweego --env-file .env --volume "${DOCKER_SHARED_FOLDER}":"/app/shared" maxfrax/soweego:latest /bin/bash
-    exit 0
-fi
+credentials_path="soweego/importer/resources/db_credentials.json"
 
-echo "Running soweego with given database credentials"
-if [ ! -f "$old_credentials" ]
-then
-    mv soweego/importer/resources/db_credentials.json $old_credentials
-fi
-cp $1 soweego/importer/resources/db_credentials.json
+PROGNAME=$0
+
+usage() {
+  cat << EOF >&2
+Usage: $PROGNAME [-s <dir>] [-c <file>] 
+
+-s <dir>: ...
+-f <file>: ...
+
+EOF
+  exit 1
+}
+
+export DOCKER_SHARED_FOLDER="/tmp/soweego_shared/"
+git checkout $credentials_path
+
+while getopts :s:c: o; do
+  case $o in
+    (s) export DOCKER_SHARED_FOLDER="$OPTARG";;
+    (c) cp $OPTARG $credentials_path;;
+    (*) usage
+  esac
+done
+shift "$((OPTIND - 1))"
+
+
 docker build --rm -f "Dockerfile.prod" -t maxfrax/soweego:latest .
 docker run -it --rm --name soweego --env-file .env --volume "${DOCKER_SHARED_FOLDER}":"/app/shared" maxfrax/soweego:latest /bin/bash
--- scripts/docker/launch_test.sh
+++ scripts/docker/launch_test.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+PROGNAME=$0
+
+usage() {
+  cat << EOF >&2
+Usage: $PROGNAME [-s <dir>]
+
+-s <dir>: ...
+
+EOF
+  exit 1
+}
+
+export DOCKER_SHARED_FOLDER="/tmp/soweego_shared/"
+
+while getopts :s: o; do
+  case $o in
+    (s) export DOCKER_SHARED_FOLDER="$OPTARG";;
+    (*) usage
+  esac
+done
+shift "$((OPTIND - 1))"
 
 docker-compose -f "docker-compose.dev.yml" up -d --build
 docker-compose -f "docker-compose.dev.yml" exec soweego /bin/bash
