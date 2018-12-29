99c56728af7a80d9e2bd7e468efe97b751d1945c
Marco Fossati <fossati@fbk.eu>
2018-12-29 15:58:46 +0100

    remove code for remote debugging

--- soweego/__init__.py
+++ soweego/__init__.py
@@ -1,9 +1,1 @@
 from soweego import commons, importer, ingestor, linker, validator, wikidata
-
-# import ptvsd
-
-# # Allow other computers to attach to ptvsd at this IP address and port.
-# ptvsd.enable_attach(address=('0.0.0.0', 3000), redirect_output=True)
-
-# # Pause the program until a remote debugger is attached
-# ptvsd.wait_for_attach()
