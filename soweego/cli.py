1fc0be62a9d22d2403fb06872a88c376c6fe6261
Marco Fossati <fossati@fbk.eu>
2019-01-14 11:05:41 +0100

    set default log level to DEBUG

--- soweego/cli.py
+++ soweego/cli.py
@@ -32,7 +32,8 @@ logging.getLogger("requests").setLevel(logging.WARNING)
 @click.option('-l', '--log-level',
               type=(str, click.Choice(commons.logging.LEVELS)),
               multiple=True,
-              help='Module name followed by one of [DEBUG, INFO, WARNING, ERROR, CRITICAL].')
+              default=('soweego', 'DEBUG')
+              help='Module name followed by one of [DEBUG, INFO, WARNING, ERROR, CRITICAL]. Multiple pairs allowed.')
 @click.pass_context
 def cli(ctx, log_level):
     """Link Wikidata items to trusted external catalogs."""
