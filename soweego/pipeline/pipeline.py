3e0398555217c47315fd82049b4b2a0bb655a1a4
Massimo Frasson <maxfrax@gmail.com>
2019-01-15 16:22:22 +0100

    All the steps can be enabled or disabled while launching the pipeline

--- soweego/pipeline/pipeline.py
+++ soweego/pipeline/pipeline.py
@@ -7,21 +7,22 @@ from soweego.importer.importer import import_cli
 @click.command()
 @click.argument('target', type=click.Choice(target_database.available_targets()))
 @click.option('--validator/--no-validator', default=False, help='Executes the validation steps for the target. Default: no.')
-@click.option('--validator-only/--all', default=False, help='Excludes from execution non validator steps. Default: no.')
+@click.option('--importer/--no-importer', default=True, help='Executes the importer steps for the target. Default: yes.')
+@click.option('--linker/--no-linker', default=True, help='Executes the linker steps for the target. Default: yes.')
 @click.option('-c', '--credentials-path', type=click.Path(file_okay=True), default=None,
               help="default: None")
 @click.option('-o', '--output-dir', type=click.Path(file_okay=False), default='/app/shared',
               help="default: '/app/shared'")
-def pipeline(target: str, validator: bool, validator_only: bool, credentials_path: str, output_dir: str):
+def pipeline(target: str, validator: bool, importer: bool, linker: bool, credentials_path: str, output_dir: str):
     """Executes importer/linker and optionally validator for a target"""
 
     if credentials_path:
         DBManager.set_credentials_from_path(credentials_path)
 
-    if not validator_only:
+    if importer:
         _importer(target, output_dir)
+    if linker:
         _linker(target, output_dir)
-
     if validator:
         _validator(target, output_dir)
 
