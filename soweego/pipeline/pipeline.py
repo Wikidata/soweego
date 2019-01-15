53a1c93028be352d7ab0f1e5a96369357de0012b
Massimo Frasson <maxfrax@gmail.com>
2019-01-15 16:10:24 +0100

    Function from stackoverflow to execute click commands

--- soweego/pipeline/pipeline.py
+++ soweego/pipeline/pipeline.py
@@ -31,3 +31,47 @@ def _linker(target: str, output_dir: str):
 
 def _validator(target: str, output_dir: str):
     return
+
+
+# Calls click commands
+# Copied from https://stackoverflow.com/questions/48619517/call-a-click-command-from-code
+def call_click_command(cmd, *args, **kwargs):
+    """ Wrapper to call a click command
+
+    :param cmd: click cli command function to call 
+    :param args: arguments to pass to the function 
+    :param kwargs: keywrod arguments to pass to the function 
+    :return: None 
+    """
+
+    # Get positional arguments from args
+    arg_values = {c.name: a for a, c in zip(args, cmd.params)}
+    args_needed = {c.name: c for c in cmd.params
+                   if c.name not in arg_values}
+
+    # build and check opts list from kwargs
+    opts = {a.name: a for a in cmd.params if isinstance(a, click.Option)}
+    for name in kwargs:
+        if name in opts:
+            arg_values[name] = kwargs[name]
+        else:
+            if name in args_needed:
+                arg_values[name] = kwargs[name]
+                del args_needed[name]
+            else:
+                raise click.BadParameter(
+                    "Unknown keyword argument '{}'".format(name))
+
+    # check positional arguments list
+    for arg in (a for a in cmd.params if isinstance(a, click.Argument)):
+        if arg.name not in arg_values:
+            raise click.BadParameter("Missing required positional"
+                                     "parameter '{}'".format(arg.name))
+
+    # build parameter lists
+    opts_list = sum(
+        [[o.opts[0], str(arg_values[n])] for n, o in opts.items()], [])
+    args_list = [str(v) for n, v in arg_values.items() if n not in opts]
+
+    # call the command
+    cmd(opts_list + args_list)
