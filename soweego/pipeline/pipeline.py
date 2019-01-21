2e4b0d6147211269d8bfd102e348331084855631
Massimo Frasson <maxfrax@gmail.com>
2019-01-21 18:02:13 +0100

    removes unused call_click_command

--- soweego/pipeline/pipeline.py
+++ soweego/pipeline/pipeline.py
@@ -2,7 +2,8 @@ import click
 from soweego.commons import target_database
 from soweego.commons.db_manager import DBManager
 from soweego.importer.importer import import_cli
-from soweego.validator.checks import check_existence_cli, check_links_cli, check_metadata_cli
+from soweego.validator.checks import (check_existence_cli, check_links_cli,
+                                      check_metadata_cli)
 
 
 @click.command()
@@ -12,77 +13,31 @@ from soweego.validator.checks import check_existence_cli, check_links_cli, check
 @click.option('--linker/--no-linker', default=True, help='Executes the linker steps for the target. Default: yes.')
 @click.option('-c', '--credentials-path', type=click.Path(file_okay=True), default=None,
               help="default: None")
-@click.option('-o', '--output-dir', type=click.Path(file_okay=False), default='/app/shared',
-              help="default: '/app/shared'")
-def pipeline(target: str, validator: bool, importer: bool, linker: bool, credentials_path: str, output_dir: str):
+def pipeline(target: str, validator: bool, importer: bool, linker: bool, credentials_path: str):
     """Executes importer/linker and optionally validator for a target"""
 
     if credentials_path:
         DBManager.set_credentials_from_path(credentials_path)
 
     if importer:
-        _importer(target, output_dir)
+        _importer(target)
     if linker:
-        _linker(target, output_dir)
+        _linker(target)
     if validator:
-        _validator(target, output_dir)
+        _validator(target)
 
 
-def _importer(target: str, output_dir: str):
-    call_click_command(import_cli, target, '--output', output_dir)
+def _importer(target: str):
+    import_cli([target])
 
 
-def _linker(target: str, output_dir: str):
+def _linker(target: str):
     return
 
 
-def _validator(target: str, output_dir: str):
+def _validator(target: str):
     # Runs the validator for each kind of entity of the given target database
     for entity_type in target_database.available_types_for_target(target):
-        call_click_command(check_existence_cli, entity_type, target)
-        call_click_command(check_links_cli, entity_type, target)
-        call_click_command(check_metadata_cli, entity_type, target)
-
-
-# Calls click commands
-# Copied from https://stackoverflow.com/questions/48619517/call-a-click-command-from-code
-def call_click_command(cmd, *args, **kwargs):
-    """ Wrapper to call a click command
-
-    :param cmd: click cli command function to call 
-    :param args: arguments to pass to the function 
-    :param kwargs: keywrod arguments to pass to the function 
-    :return: None 
-    """
-
-    # Get positional arguments from args
-    arg_values = {c.name: a for a, c in zip(args, cmd.params)}
-    args_needed = {c.name: c for c in cmd.params
-                   if c.name not in arg_values}
-
-    # build and check opts list from kwargs
-    opts = {a.name: a for a in cmd.params if isinstance(a, click.Option)}
-    for name in kwargs:
-        if name in opts:
-            arg_values[name] = kwargs[name]
-        else:
-            if name in args_needed:
-                arg_values[name] = kwargs[name]
-                del args_needed[name]
-            else:
-                raise click.BadParameter(
-                    "Unknown keyword argument '{}'".format(name))
-
-    # check positional arguments list
-    for arg in (a for a in cmd.params if isinstance(a, click.Argument)):
-        if arg.name not in arg_values:
-            raise click.BadParameter("Missing required positional"
-                                     "parameter '{}'".format(arg.name))
-
-    # build parameter lists
-    opts_list = sum(
-        [[o.opts[0], str(arg_values[n])] for n, o in opts.items()], [])
-    args_list = [str(v) for n, v in arg_values.items() if n not in opts]
-
-    # call the command
-    cmd(opts_list + args_list)
+        check_existence_cli([entity_type, target])
+        check_links_cli([entity_type, target])
+        check_metadata_cli([entity_type, target])
