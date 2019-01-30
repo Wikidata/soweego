3162ec219d3807e3b60d232cf21beb8b88a8f474
Marco Fossati <fossati@fbk.eu>
2019-01-30 09:54:36 +0100

    not necessarily <name> <surname>

--- soweego/importer/models/base_entity.py
+++ soweego/importer/models/base_entity.py
@@ -22,7 +22,7 @@ class BaseEntity(AbstractConcreteBase, BASE):
                          primary_key=True, autoincrement=True)
     # Catalog identifier, indexed
     catalog_id = Column(String(50), nullable=False, index=True)
-    # Full name (<name> <surname>)
+    # Full name
     name = Column(String(255), nullable=False)
     # Tokenized full name, can be null. See text_utils#tokenize
     name_tokens = Column(String(255))
