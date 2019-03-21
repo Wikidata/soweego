e5ed49d7b582da059115c69eba2887479a61851b
Marco Fossati <fossati@fbk.eu>
2019-03-21 12:57:29 +0100

    refactor input dataset (feature vectors) construction

--- soweego/linker/train.py
+++ soweego/linker/train.py
@@ -11,10 +11,8 @@ __copyright__ = 'Copyleft 2018, Hjfocs'
 
 import logging
 import os
-from itertools import tee
 
 import click
-import recordlinkage as rl
 from pandas import MultiIndex, concat
 from sklearn.externals import joblib
 
@@ -42,8 +40,17 @@ def cli(classifier, target, target_type, binarize, dir_io):
 
 
 def execute(classifier, catalog, entity, binarize, dir_io):
-    wd_reader = workflow.build_wikidata('training', catalog, entity, dir_io)
-    wd_generator = workflow.preprocess_wikidata('training', wd_reader)
+    goal = 'training'
+
+    feature_vectors, positive_samples_index = build_dataset(
+        goal, catalog, entity, dir_io)
+
+    return _train(classifier, feature_vectors, positive_samples_index, binarize)
+
+
+def build_dataset(goal, catalog, entity, dir_io):
+    wd_reader = workflow.build_wikidata(goal, catalog, entity, dir_io)
+    wd_generator = workflow.preprocess_wikidata(goal, wd_reader)
 
     positive_samples, feature_vectors = [], []
 
@@ -53,27 +60,28 @@ def execute(classifier, catalog, entity, binarize, dir_io):
 
         # Samples index from Wikidata
         all_samples = blocking.full_text_query_block(
-            'training', catalog, wd_chunk[constants.NAME_TOKENS], i, target_database.get_entity(catalog, entity), dir_io)
+            goal, catalog, wd_chunk[constants.NAME_TOKENS], i, target_database.get_entity(catalog, entity), dir_io)
 
         # Build target chunk based on samples
         target_reader = data_gathering.gather_target_dataset(
-            'training', entity, catalog, set(all_samples.get_level_values(constants.TID)))
+            goal, entity, catalog, set(all_samples.get_level_values(constants.TID)))
 
         # Preprocess target chunk
-        target_chunk = workflow.preprocess_target('training', target_reader)
+        target_chunk = workflow.preprocess_target(
+            goal, target_reader)
 
         features_path = os.path.join(
-            dir_io, constants.FEATURES % (catalog, entity, 'training', i))
-            
+            dir_io, constants.FEATURES % (catalog, entity, goal, i))
+
         feature_vectors.append(workflow.extract_features(
             all_samples, wd_chunk, target_chunk, features_path))
 
     positive_samples = concat(positive_samples)
     positive_samples_index = MultiIndex.from_tuples(zip(
         positive_samples.index, positive_samples), names=[constants.QID, constants.TID])
-    LOGGER.info('Built positive samples index from Wikidata')
 
-    return _train(classifier, concat(feature_vectors), positive_samples_index, binarize)
+    LOGGER.info('Built positive samples index from Wikidata')
+    return concat(feature_vectors), positive_samples_index
 
 
 def _build_positive_samples_index(wd_reader1):
