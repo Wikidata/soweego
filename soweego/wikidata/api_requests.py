5846ecca6c4c102b4ab29cdf2b707aef2a231a66
Andrea Tupini <tupini07@gmail.com>
2019-03-22 09:27:14 +0100

    display a message when unauthenticated session is used

--- soweego/wikidata/api_requests.py
+++ soweego/wikidata/api_requests.py
@@ -475,6 +475,7 @@ def get_authenticated_session():
 
             if bot_password == '':
                 # maximum bucket size when authenticated is 50
+                LOGGER.info('No password provided so unauthenticated session will be used')
                 BUCKET_SIZE = 50 
                 break
 
