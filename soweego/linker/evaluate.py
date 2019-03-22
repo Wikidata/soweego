a3bc9a5fef55986272c07fa356735ef097e48e38
Marco Fossati <fossati@fbk.eu>
2019-03-22 10:53:23 +0100

    mean + std evaluation scores over k-folds as the default evaluation

--- soweego/linker/evaluate.py
+++ soweego/linker/evaluate.py
@@ -7,13 +7,14 @@ __author__ = 'Marco Fossati'
 __email__ = 'fossati@spaziodati.eu'
 __version__ = '1.0'
 __license__ = 'GPL-3.0'
-__copyright__ = 'Copyleft 2018, Hjfocs'
+__copyright__ = 'Copyleft 2019, Hjfocs'
 
 import logging
 import os
 
 import click
 import recordlinkage as rl
+from numpy import mean, std
 from pandas import concat
 from sklearn.model_selection import KFold, train_test_split
 
@@ -21,26 +22,40 @@ from soweego.commons import constants, target_database
 from soweego.linker import train, workflow
 
 LOGGER = logging.getLogger(__name__)
-rl.set_option(*constants.CLASSIFICATION_RETURN_SERIES)
 
 
 @click.command()
 @click.argument('classifier', type=click.Choice(constants.CLASSIFIERS))
 @click.argument('target', type=click.Choice(target_database.available_targets()))
 @click.argument('target_type', type=click.Choice(target_database.available_types()))
-@click.option('-b', '--binarize', default=0.1, help="Default: 0.1")
+@click.option('--single', is_flag=True, help='Compute a single evaluation over all k folds, instead of k evaluations.')
+@click.option('-b', '--binarize', default=0.1, help="Default: 0.1.")
 @click.option('-d', '--dir-io', type=click.Path(file_okay=False), default='/app/shared', help="Input/output directory, default: '/app/shared'.")
-def cli(classifier, target, target_type, binarize, dir_io):
+def cli(classifier, target, target_type, binarize, single, dir_io):
     """Evaluate the performance of a probabilistic linker."""
-    precision, recall, fscore, confusion_matrix = k_fold_single_evaluation(
-        constants.CLASSIFIERS[classifier], target, target_type, binarize, dir_io)
-    with open(os.path.join(dir_io, constants.LINKER_PERFORMANCE % (target, target_type, classifier)), 'w') as fileout:
-        fileout.write(
-            f'Precision: {precision}\nRecall: {recall}\nF-score: {fscore}\nConfusion matrix:\n{confusion_matrix}\n')
+    if not single:
+        predictions, p_mean, p_std, r_mean, r_std, fscore_mean, fscore_std = average_k_fold(
+            constants.CLASSIFIERS[classifier], target, target_type, binarize, dir_io)
+        LOGGER.info('Precision: mean = %s; std = %s; Recall: mean = %s; std = %s; F-score: mean = %s; std = %s',
+                    p_mean, p_std, r_mean, r_std, fscore_mean, fscore_std)
+
+        predictions.to_series().to_csv(os.path.join(dir_io, constants.LINKER_EVALUATION_PREDICTIONS %
+                                                    (target, target_type, classifier)), columns=[], header=True)
+        with open(os.path.join(dir_io, constants.LINKER_PERFORMANCE % (target, target_type, classifier)), 'w') as fileout:
+            fileout.write(
+                f'Precision:\n\tmean = {p_mean}\n\tstandard deviation = {p_std}\nRecall:\n\tmean = {r_mean}\n\tstandard deviation = {r_std}\nF-score:\n\tmean = {fscore_mean}\n\tstandard deviation = {fscore_std}\n')
+    else:
+        predictions, (precision, recall, fscore, confusion_matrix) = single_k_fold(
+            constants.CLASSIFIERS[classifier], target, target_type, binarize, dir_io)
+
+        predictions.to_series().to_csv(os.path.join(dir_io, constants.LINKER_EVALUATION_PREDICTIONS %
+                                                    (target, target_type, classifier)), columns=[], header=True)
+        with open(os.path.join(dir_io, constants.LINKER_PERFORMANCE % (target, target_type, classifier)), 'w') as fileout:
+            fileout.write(
+                f'Precision: {precision}\nRecall: {recall}\nF-score: {fscore}\nConfusion matrix:\n{confusion_matrix}\n')
 
 
 def _compute_performance(test_index, predictions, test_vectors_size):
-
     LOGGER.info('Running performance evaluation ...')
 
     confusion_matrix = rl.confusion_matrix(
@@ -56,8 +71,33 @@ def _compute_performance(test_index, predictions, test_vectors_size):
     return precision, recall, fscore, confusion_matrix
 
 
-def k_fold_single_evaluation(classifier, catalog, entity, binarize, dir_io, k=5):
-    predictions, test_set = [], []
+def average_k_fold(classifier, catalog, entity, binarize, dir_io, k=5):
+    predictions, precisions, recalls, fscores = None, [], [], []
+    dataset, positive_samples_index = train.build_dataset(
+        'training', catalog, entity, dir_io)
+    k_fold = KFold(n_splits=k, shuffle=True)
+
+    for train_index, test_index in k_fold.split(dataset):
+        training, test = dataset.iloc[train_index], dataset.iloc[test_index]
+        model = workflow.init_model(classifier, binarize)
+        model.fit(training, positive_samples_index & training.index)
+        preds = model.predict(test)
+        p, r, f, _ = _compute_performance(
+            positive_samples_index & test.index, preds, len(test))
+
+        if predictions is None:
+            predictions = preds
+        else:
+            predictions |= preds
+        precisions.append(p)
+        recalls.append(r)
+        fscores.append(f)
+
+    return predictions, mean(precisions), std(precisions), mean(recalls), std(recalls), mean(fscores), std(fscores)
+
+
+def single_k_fold(classifier, catalog, entity, binarize, dir_io, k=5):
+    predictions, test_set = None, []
     dataset, positive_samples_index = train.build_dataset(
         'training', catalog, entity, dir_io)
     k_fold = KFold(n_splits=k, shuffle=True)
@@ -67,10 +107,13 @@ def k_fold_single_evaluation(classifier, catalog, entity, binarize, dir_io, k=5)
         test_set.append(test)
         model = workflow.init_model(classifier, binarize)
         model.fit(training, positive_samples_index & training.index)
-        predictions.append(model.predict(test))
+        if predictions is None:
+            predictions = model.predict(test)
+        else:
+            predictions |= model.predict(test)
 
     test_set = concat(test_set)
-    return _compute_performance(positive_samples_index & test_set.index, concat(predictions), len(test_set))
+    return predictions, _compute_performance(positive_samples_index & test_set.index, predictions, len(test_set))
 
 
 def _random_split(wd_chunk, target_chunk):
