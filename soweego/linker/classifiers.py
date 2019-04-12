d0f09d54191f2b18d8b5071c85c8ebf31723fb7b
Andrea Tupini <tupini07@gmail.com>
2019-04-12 11:34:02 +0200

    SVC prob method now returns pd.Dataframe with correct index
    ref #269

--- soweego/linker/classifiers.py
+++ soweego/linker/classifiers.py
@@ -11,6 +11,7 @@ __copyright__ = 'Copyleft 2019, tupini07'
 
 import logging
 
+import pandas as pd
 from recordlinkage.adapters import SKLearnAdapter
 from recordlinkage.base import BaseClassifier
 from sklearn.svm import SVC
@@ -28,6 +29,20 @@ class SVCClassifier(SKLearnAdapter, BaseClassifier):
         self.kernel = SVC(*args, **kwargs)
 
     def prob(self, x):
-        import ipdb
-        ipdb.set_trace()
-        return self.kernel.predict_proba(x)
+
+        # The `predict_proba` method of the SVC classifier
+        # returns a matrix where each row corresponds to
+        # one element, and the columns correspont to the classes
+        # the classifier was trained on. In our case it was
+        # trained only on 2 [0,1]. Since we only want the
+        # probability that the elements match then we return
+        # only the second column, which is the probability
+        # of belonging to the class 1
+
+        assert self.kernel.classes_[1] == 1, ('Invalid classes, the SVC predict probability '
+                                              'expects that the second class in the trained '
+                                              f'model is 1. It currently is: {self.kernel.classes_[1]}')
+        
+        probabilities = self.kernel.predict_proba(x)[:, 1]
+        
+        return pd.DataFrame(probabilities, index=x.index)
