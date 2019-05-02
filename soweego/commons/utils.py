627ca003dd56621856b2fa2234411dff10115812
Marco Fossati <fossati@fbk.eu>
2019-05-02 18:45:54 +0200

    move shared functions & SLP model building for grid search (WIP)

--- soweego/commons/utils.py
+++ soweego/commons/utils.py
@@ -11,7 +11,32 @@ __copyright__ = 'Copyleft 2018, Hjfocs'
 
 import logging
 
+from keras.layers import Dense
+from keras.models import Sequential
+from sklearn.model_selection import StratifiedKFold
+
+from soweego.commons import constants
+from soweego.linker import neural_networks
+
 LOGGER = logging.getLogger(__name__)
+
+
+def handle_extra_cli_args(args):
+    kwargs = {}
+    for extra_arg in args:
+        try:
+            key, value = extra_arg.split('=')
+            try:
+                # Try to cast values that should be floats
+                kwargs[key] = float(value)
+            except ValueError:
+                # Fall back to string if the value can't be cast
+                kwargs[key] = value
+        except ValueError:
+            LOGGER.critical(
+                "Bad format for extra argument '%s'. It should be 'argument=value'", extra_arg)
+            return None
+    return kwargs
 
 
 def make_buckets(dataset, bucket_size=1000):
@@ -21,3 +46,24 @@ def make_buckets(dataset, bucket_size=1000):
     LOGGER.info('Made %s buckets of size %s from a dataset of size %s',
                 len(buckets), bucket_size, len(dataset))
     return buckets
+
+
+def prepare_stratified_k_fold(k, dataset, positive_samples_index):
+    k_fold = StratifiedKFold(n_splits=k, shuffle=True, random_state=610)
+    # scikit's stratified k-fold no longer supports multi-label data representation.
+    # It expects a binary array instead, so build it based on the positive samples index
+    binary_target_variables = dataset.index.map(
+        lambda x: 1 if x in positive_samples_index else 0)
+    return k_fold, binary_target_variables
+
+
+def build_single_layer_perceptron(input_dim):
+    model = Sequential()
+    model.add(
+        Dense(1, input_dim=input_dim, activation=constants.ACTIVATION))
+    model.compile(
+        optimizer=constants.OPTIMIZER,
+        loss=constants.LOSS,
+        metrics=constants.METRICS
+    )
+    return model
