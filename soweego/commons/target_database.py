a865bf1710fd0759e793d06e4d2d481e9757eb0f
Marco Fossati <fossati@fbk.eu>
2019-05-10 11:18:30 +0200

    get relationship DB entity & remove hardcoded constants & rename functions

--- soweego/commons/target_database.py
+++ soweego/commons/target_database.py
@@ -1,38 +1,46 @@
-from soweego.commons.constants import TARGET_CATALOGS
+from soweego.commons import constants
 from soweego.wikidata import vocabulary
 
 
 def available_targets():
-    return TARGET_CATALOGS.keys()
+    return constants.TARGET_CATALOGS.keys()
 
 
 def available_types():
     result = []
-    for key in TARGET_CATALOGS:
-        for k in TARGET_CATALOGS[key].keys():
+    for key in constants.TARGET_CATALOGS:
+        for k in constants.TARGET_CATALOGS[key].keys():
             result.append(k)
     return list(set(result))
 
 
 def available_types_for_target(target):
-    return TARGET_CATALOGS[target].keys()
+    return constants.TARGET_CATALOGS[target].keys()
 
 
-def get_entity(target, entity_type):
-    return TARGET_CATALOGS[target][entity_type]['entity']
+def get_main_entity(target, entity_type):
+    return constants.TARGET_CATALOGS[target][entity_type][constants.MAIN_ENTITY]
 
 
 def get_link_entity(target, entity_type):
-    return TARGET_CATALOGS[target][entity_type]['link_entity']
+    return constants.TARGET_CATALOGS[target][entity_type][constants.LINK_ENTITY]
 
 
 def get_nlp_entity(target, entity_type):
-    return TARGET_CATALOGS[target][entity_type]['nlp_entity']
+    return constants.TARGET_CATALOGS[target][entity_type][constants.NLP_ENTITY]
+
+
+def get_relationship_entity(target, entity_type):
+    return constants.TARGET_CATALOGS[target][entity_type][constants.RELATIONSHIP_ENTITY]
 
 
 def get_qid(catalog):
-    return vocabulary.CATALOG_MAPPING.get(catalog)['qid']
+    return vocabulary.CATALOG_MAPPING.get(catalog)[constants.QID]
 
 
-def get_pid(catalog):
-    return vocabulary.CATALOG_MAPPING.get(catalog)['pid']
+def get_person_pid(catalog):
+    return vocabulary.CATALOG_MAPPING.get(catalog)[constants.PERSON_PID]
+
+
+def get_work_pid(catalog):
+    return vocabulary.CATALOG_MAPPING.get(catalog)[constants.WORK_PID]
