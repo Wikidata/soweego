6b6bd0167626718e0796af40c149ed2cea110182
Marco Fossati <fossati@fbk.eu>
2019-05-13 17:11:17 +0200

    rename functions & add code header & refactor constants

--- soweego/commons/target_database.py
+++ soweego/commons/target_database.py
@@ -1,12 +1,24 @@
-from soweego.commons import constants
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Getters for supported catalogs constants"""
+
+__author__ = 'Marco Fossati'
+__email__ = 'fossati@spaziodati.eu'
+__version__ = '1.0'
+__license__ = 'GPL-3.0'
+__copyright__ = 'Copyleft 2019, Hjfocs'
+
+
+from soweego.commons import constants, keys
 from soweego.wikidata import vocabulary
 
 
-def available_targets():
+def supported_targets():
     return constants.TARGET_CATALOGS.keys()
 
 
-def available_types():
+def supported_entities():
     result = []
     for key in constants.TARGET_CATALOGS:
         for k in constants.TARGET_CATALOGS[key].keys():
@@ -14,33 +26,37 @@ def available_types():
     return list(set(result))
 
 
-def available_types_for_target(target):
+def supported_entities_for_target(target):
     return constants.TARGET_CATALOGS[target].keys()
 
 
 def get_main_entity(target, entity_type):
-    return constants.TARGET_CATALOGS[target][entity_type][constants.MAIN_ENTITY]
+    return constants.TARGET_CATALOGS[target][entity_type][keys.MAIN_ENTITY]
 
 
 def get_link_entity(target, entity_type):
-    return constants.TARGET_CATALOGS[target][entity_type][constants.LINK_ENTITY]
+    return constants.TARGET_CATALOGS[target][entity_type][keys.LINK_ENTITY]
 
 
 def get_nlp_entity(target, entity_type):
-    return constants.TARGET_CATALOGS[target][entity_type][constants.NLP_ENTITY]
+    return constants.TARGET_CATALOGS[target][entity_type][keys.NLP_ENTITY]
 
 
 def get_relationship_entity(target, entity_type):
-    return constants.TARGET_CATALOGS[target][entity_type][constants.RELATIONSHIP_ENTITY]
+    return constants.TARGET_CATALOGS[target][entity_type][keys.RELATIONSHIP_ENTITY]
 
 
-def get_qid(catalog):
-    return vocabulary.CATALOG_MAPPING.get(catalog)[constants.QID]
+def get_person_qid(catalog):
+    return vocabulary.CATALOG_MAPPING.get(catalog)[keys.PERSON_QID]
+
+
+def get_work_qid(catalog):
+    return vocabulary.CATALOG_MAPPING.get(catalog)[keys.WORK_QID]
 
 
 def get_person_pid(catalog):
-    return vocabulary.CATALOG_MAPPING.get(catalog)[constants.PERSON_PID]
+    return vocabulary.CATALOG_MAPPING.get(catalog)[keys.PERSON_PID]
 
 
 def get_work_pid(catalog):
-    return vocabulary.CATALOG_MAPPING.get(catalog)[constants.WORK_PID]
+    return vocabulary.CATALOG_MAPPING.get(catalog)[keys.WORK_PID]
