a3cc64142ee2af4280521afd51fe13f97b7a1127
hjfocs <hjfocs@soweego-1.soweego.eqiad.wmflabs>
2019-05-28 12:00:48 +0000

    Fixes pipelines shader folder path

--- scripts/cron/pipeline-discogs-tmux.sh
+++ scripts/cron/pipeline-discogs-tmux.sh
@@ -1,3 +1,3 @@
 cd /srv/dryrun/soweego/
 /usr/bin/tmux kill-session -t pipeline-discogs
-/usr/bin/tmux new-session -d -s "pipeline-discogs" ./scripts/docker/launch_pipeline.sh -c ../prod_cred.json -s /srv/dryrun/shared/ discogs --no-upload --validator
+/usr/bin/tmux new-session -d -s "pipeline-discogs" ./scripts/docker/launch_pipeline.sh -c ../prod_cred.json -s /srv/dryrun/discogs-shared/ discogs --no-upload --validator
--- scripts/cron/pipeline-musicbrainz-tmux.sh
+++ scripts/cron/pipeline-musicbrainz-tmux.sh
@@ -1,3 +1,3 @@
 cd /srv/dryrun/soweego/
 /usr/bin/tmux kill-session -t pipeline-musicbrainz
-/usr/bin/tmux new-session -d -s "pipeline-musicbrainz" ./scripts/docker/launch_pipeline.sh -c ../prod_cred.json -s /srv/dryrun/shared/ musicbrainz --no-upload --validator
+/usr/bin/tmux new-session -d -s "pipeline-musicbrainz" ./scripts/docker/launch_pipeline.sh -c ../prod_cred.json -s /srv/dryrun/musicbrainz-shared/ musicbrainz --no-upload --validator
