349f1f9c106c26d771563b9d0306fae63fa700f0
hjfocs <hjfocs@soweego-1.soweego.eqiad.wmflabs>
2019-05-30 14:25:47 +0000

    Fixes cron jobs

--- scripts/cron/pipeline-musicbrainz-tmux.sh
+++ scripts/cron/pipeline-musicbrainz-tmux.sh
@@ -1,4 +1,4 @@
 cd /srv/dryrun/soweego/
 /usr/bin/tmux kill-session -t pipeline-musicbrainz
-/usr/bin/tmux new-session -d -s "pipeline-musicbrainz" ./scripts/docker/launch_pipeline.sh -c ../prod_cred.json -s /srv/dryrun/musicbrainz-shared/ musicbrainz --no-upload --validator
-/usr/bin/tmux set remain-on-exit on -t pipeline-musicbrainz
+/usr/bin/tmux new-session -d -s "pipeline-musicbrainz"
+/usr/bin/tmux send-keys -t pipeline-musicbrainz:0 "./scripts/docker/launch_pipeline.sh -c ../prod_cred.json -s /srv/dryrun/musicbrainz-shared/ musicbrainz --no-upload --validator" ENTER
