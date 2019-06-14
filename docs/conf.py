c72ea3c862f1cfe87a25911ba3c70d63489ec3bb
Andrea Tupini <tupini07@gmail.com>
2019-06-14 17:27:34 +0200

    add intersphinx extension for documentation

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,8 @@ release = '1.0'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx_autodoc_typehints'
+    'sphinx_autodoc_typehints',
+    'sphinx.ext.intersphinx'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
