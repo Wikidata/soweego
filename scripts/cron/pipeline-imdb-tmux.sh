9a91246dd9d6f2b737b034e272c980be9c1d1a6c
root <root@soweego.soweego.eqiad.wmflabs>
2019-06-21 12:41:59 +0000

    Fixes cronjobs

--- scripts/cron/pipeline-imdb-tmux.sh
+++ scripts/cron/pipeline-imdb-tmux.sh
@@ -1,4 +1,4 @@
-cd /srv/dryrun/soweego/
+cd /srv/soweego/
 /usr/bin/tmux kill-session -t pipeline-imdb
 /usr/bin/tmux new-session -d -s "pipeline-imdb"
-/usr/bin/tmux send-keys -t pipeline-imdb:0 "./scripts/docker/launch_pipeline.sh -c ../prod_cred.json -s /srv/dryrun/imdb-shared/ imdb --no-upload --validator" ENTER
+/usr/bin/tmux send-keys -t pipeline-imdb:0 "./scripts/docker/launch_pipeline.sh -c ../prod_cred.json -s /srv/imdb-shared/ imdb --no-upload --validator" ENTER
