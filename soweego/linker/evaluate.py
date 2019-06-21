db005e86aa88d250f8528131e7dd45eba9bf322d
Marco Fossati <fossati@fbk.eu>
2019-06-21 18:02:21 +0200

    refactor & document & improve code readability of CLI command

--- soweego/linker/evaluate.py
+++ soweego/linker/evaluate.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""Evaluation of supervised linking."""
+"""Evaluation of supervised linking algorithms."""
 
 __author__ = 'Marco Fossati'
 __email__ = 'fossati@spaziodati.eu'
@@ -33,162 +33,116 @@ from soweego.linker import train, workflow
 LOGGER = logging.getLogger(__name__)
 
 
+# Let the user pass extra kwargs to the classifier
+# This is for development purposes only, and is not explicitly documented
 @click.command(
     context_settings={'ignore_unknown_options': True, 'allow_extra_args': True}
 )
 @click.argument('classifier', type=click.Choice(constants.CLASSIFIERS))
 @click.argument(
-    'target', type=click.Choice(target_database.supported_targets())
+    'catalog', type=click.Choice(target_database.supported_targets())
 )
 @click.argument(
-    'target_type', type=click.Choice(target_database.supported_entities())
-)
-@click.option(
-    '--nested',
-    is_flag=True,
-    help='Compute a nested cross-validation with hyperparameters tuning via grid search.',
-)
-@click.option(
-    '--single',
-    is_flag=True,
-    help='Compute a single evaluation over all k folds, instead of k evaluations.',
+    'entity', type=click.Choice(target_database.supported_entities())
 )
 @click.option('-k', '--k-folds', default=5, help="Number of folds, default: 5.")
+@click.option(
+    '-s',
+    '--single',
+    is_flag=True,
+    help='Compute a single evaluation over all k folds, instead of k '
+         'evaluations.',
+)
+@click.option(
+    '-n',
+    '--nested',
+    is_flag=True,
+    help='Compute a nested cross-validation with hyperparameters tuning via '
+         'grid search. WARNING: this will take a lot of time.',
+)
 @click.option(
     '-m',
     '--metric',
     type=click.Choice(constants.PERFORMANCE_METRICS),
     default='f1',
-    help="Performance metric for nested cross-validation. Use with '--nested'. Default: f1.",
+    help="Performance metric for nested cross-validation. "
+         "Use with '--nested'. Default: f1.",
 )
 @click.option(
     '-d',
     '--dir-io',
     type=click.Path(file_okay=False),
     default=constants.SHARED_FOLDER,
-    help="Input/output directory, default: '%s'." % constants.SHARED_FOLDER,
+    help=f'Input/output directory, default: {constants.SHARED_FOLDER}.',
 )
 @click.pass_context
-def cli(
-    ctx,
-    classifier,
-    target,
-    target_type,
-    nested,
-    single,
-    k_folds,
-    metric,
-    dir_io,
-):
-    """Evaluate the performance of a probabilistic linker.
-    By default, it runs 5-fold cross-validation and returns averaged performance scores.
+def cli(ctx, classifier, catalog, entity, k_folds, single, nested, metric,
+        dir_io):
+    """Evaluate the performance of a supervised linker.
+
+    By default, run 5-fold cross-validation and
+    return averaged performance scores.
     """
     kwargs = utils.handle_extra_cli_args(ctx.args)
     if kwargs is None:
         sys.exit(1)
 
     rl.set_option(*constants.CLASSIFICATION_RETURN_INDEX)
-    performance_fileout = os.path.join(
-        dir_io, constants.LINKER_PERFORMANCE % (target, target_type, classifier)
-    )
-    predictions_fileout = os.path.join(
-        dir_io,
-        constants.LINKER_EVALUATION_PREDICTIONS
-        % (target, target_type, classifier),
-    )
 
+    performance_out, predictions_out = _build_output_paths(catalog, entity,
+                                                           classifier, dir_io)
+
+    # -n, --nested
     if nested:
-        LOGGER.warning(
-            'You have opted for the slowest evaluation option, please be patient ...'
-        )
-        LOGGER.info(
-            'Starting nested %d-fold cross validation with hyperparameters tuning via grid search ...',
-            k_folds,
-        )
-
-        clf = constants.CLASSIFIERS[classifier]
-        param_grid = constants.PARAMETER_GRIDS[clf]
-        result = nested_k_fold_with_grid_search(
-            clf,
-            param_grid,
-            target,
-            target_type,
-            k_folds,
-            metric,
-            dir_io,
-            **kwargs,
-        )
-
-        LOGGER.info('Evaluation done: %s', result)
-
-        # Persist best models
-        for k, model in enumerate(result.pop('best_models'), 1):
-            model_fileout = os.path.join(
-                dir_io,
-                constants.LINKER_NESTED_CV_BEST_MODEL
-                % (target, target_type, classifier, k),
-            )
-            result['best_models'].append(model_fileout)
-
-            joblib.dump(model, model_fileout)
-            LOGGER.info(
-                "Best model for fold %d dumped to '%s'", k, model_fileout
-            )
-
-        performance_fileout = performance_fileout.replace('txt', 'json')
-        with open(performance_fileout, 'w') as fout:
-            json.dump(result, fout, indent=2)
-        LOGGER.info(
-            "%s performance dumped to '%s'", metric, performance_fileout
-        )
+        _run_nested(classifier, catalog, entity, k_folds, metric, kwargs,
+                    performance_out, dir_io)
         sys.exit(0)
 
+    # -s, --single
     if single:
-        LOGGER.info('Starting single evaluation over %d folds ...', k_folds)
-
-        predictions, (
-            precision,
-            recall,
-            fscore,
-            confusion_matrix,
-        ) = single_k_fold(
-            constants.CLASSIFIERS[classifier],
-            target,
-            target_type,
-            k_folds,
-            dir_io,
-            **kwargs,
-        )
-
-        LOGGER.info('Evaluation done')
-
-        predictions.to_series().to_csv(predictions_fileout)
-        with open(performance_fileout, 'w') as fout:
-            fout.write(
-                f'Precision: {precision}\nRecall: {recall}\nF-score: {fscore}\nConfusion matrix:\n{confusion_matrix}\n'
-            )
-
-        LOGGER.info(
-            "Predictions dumped to '%s', Performance dumped to '%s'",
-            predictions_fileout,
-            performance_fileout,
-        )
+        _run_single(classifier, catalog, entity, k_folds, kwargs,
+                    performance_out, predictions_out, dir_io)
         sys.exit(0)
 
     # Default: average evaluation over k-fold
+    _run_average(classifier, catalog, entity, k_folds, kwargs, performance_out,
+                 predictions_out, dir_io)
+
+
+def _build_output_paths(catalog, entity, classifier, dir_io):
+    performance_outpath = os.path.join(
+        dir_io,
+        constants.LINKER_PERFORMANCE.format(catalog, entity, classifier)
+    )
+    predictions_outpath = os.path.join(
+        dir_io,
+        constants.LINKER_EVALUATION_PREDICTIONS.format(
+            catalog, entity, classifier
+        ),
+    )
+    os.makedirs(os.path.dirname(predictions_outpath), exist_ok=True)
+
+    return performance_outpath, predictions_outpath
+
+
+def _run_average(classifier, catalog, entity, k_folds, kwargs, performance_out,
+                 predictions_out, dir_io):
     LOGGER.info('Starting average evaluation over %d folds ...', k_folds)
 
     predictions, p_mean, p_std, r_mean, r_std, fscore_mean, fscore_std = average_k_fold(
         constants.CLASSIFIERS[classifier],
-        target,
-        target_type,
+        catalog,
+        entity,
         k_folds,
         dir_io,
         **kwargs,
     )
 
     LOGGER.info(
-        'Evaluation done. Precision: mean = %s; std = %s; Recall: mean = %s; std = %s; F-score: mean = %s; std = %s',
+        'Evaluation done. '
+        'Precision: mean = %s; std = %s; '
+        'recall: mean = %s; std = %s; '
+        'F-score: mean = %s; std = %s',
         p_mean,
         p_std,
         r_mean,
@@ -197,17 +151,118 @@ def cli(
         fscore_std,
     )
 
-    os.makedirs(os.path.dirname(predictions_fileout), exist_ok=True)
-    predictions.to_series().to_csv(predictions_fileout, header=False)
-    with open(performance_fileout, 'w') as fout:
-        fout.write(
-            f'Precision:\n\tmean = {p_mean}\n\tstandard deviation = {p_std}\nRecall:\n\tmean = {r_mean}\n\tstandard deviation = {r_std}\nF-score:\n\tmean = {fscore_mean}\n\tstandard deviation = {fscore_std}\n'
+    predictions.to_series().to_csv(predictions_out, header=False)
+    with open(performance_out, 'w') as out:
+        out.write(
+            f'Precision:\n'
+            f'\tmean = {p_mean}\n'
+            f'\tstandard deviation = {p_std}\n'
+            f'Recall:\n'
+            f'\tmean = {r_mean}\n'
+            f'\tstandard deviation = {r_std}\n'
+            f'F-score:\n'
+            f'\tmean = {fscore_mean}\n'
+            f'\tstandard deviation = {fscore_std}\n'
+        )
+
+    LOGGER.info(
+        "Predictions dumped to '%s', performance dumped to '%s'",
+        predictions_out,
+        performance_out,
+    )
+
+
+def _run_single(classifier, catalog, entity, k_folds, kwargs, performance_out,
+                predictions_out, dir_io):
+    LOGGER.info('Starting single evaluation over %d folds ...', k_folds)
+
+    predictions, (
+        precision,
+        recall,
+        fscore,
+        confusion_matrix,
+    ) = single_k_fold(
+        constants.CLASSIFIERS[classifier],
+        catalog,
+        entity,
+        k_folds,
+        dir_io,
+        **kwargs,
+    )
+
+    LOGGER.info(
+        'Evaluation done. '
+        'Precision: %s; '
+        'recall: %s; '
+        'F-score: %s',
+        precision, recall, fscore
+    )
+
+    predictions.to_series().to_csv(predictions_out, header=False)
+    with open(performance_out, 'w') as out:
+        out.write(
+            f'Precision: {precision}\n'
+            f'Recall: {recall}\n'
+            f'F-score: {fscore}\n'
+            f'Confusion matrix:\n{confusion_matrix}\n'
         )
 
     LOGGER.info(
         "Predictions dumped to '%s', Performance dumped to '%s'",
-        predictions_fileout,
-        performance_fileout,
+        predictions_out,
+        performance_out,
+    )
+
+
+def _run_nested(classifier, catalog, entity, k_folds, metric, kwargs,
+                performance_out, dir_io):
+    LOGGER.warning(
+        'You have opted for the slowest evaluation option, '
+        'please be patient ...'
+    )
+    LOGGER.info(
+        f'Starting nested {k_folds}-fold cross-validation with '
+        f'hyperparameters tuning via grid search ...',
+    )
+
+    clf = constants.CLASSIFIERS[classifier]
+    param_grid = constants.PARAMETER_GRIDS[clf]
+
+    result = nested_k_fold_with_grid_search(
+        clf,
+        param_grid,
+        catalog,
+        entity,
+        k_folds,
+        metric,
+        dir_io,
+        **kwargs,
+    )
+
+    LOGGER.info('Evaluation done: %s', result)
+
+    # Persist best models
+    for k, model in enumerate(result.pop('best_models'), 1):
+        model_out = os.path.join(
+            dir_io,
+            constants.LINKER_NESTED_CV_BEST_MODEL.format(
+                catalog, entity, classifier, k
+            )
+        )
+
+        result['best_models'].append(model_out)
+        joblib.dump(model, model_out)
+
+        LOGGER.info(
+            "Best model for fold %d dumped to '%s'", k, model_out
+        )
+
+    performance_out = performance_out.replace('txt', 'json')
+    with open(performance_out, 'w') as out:
+        json.dump(result, out, indent=2)
+
+    LOGGER.info(
+        "%s performance dumped to '%s'", metric, performance_out
     )
 
 
