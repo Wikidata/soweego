b695fb0e06049aad7966ef000ea0129e326960f0
Marco Fossati <fossati@fbk.eu>
2019-06-25 17:19:06 +0200

    docstring & refactor & code comments & beautify ugly function

--- soweego/linker/link.py
+++ soweego/linker/link.py
@@ -12,7 +12,7 @@ __copyright__ = 'Copyleft 2018, Hjfocs'
 
 import logging
 import os
-import re
+from re import search
 from typing import Iterator
 
 import click
@@ -95,13 +95,13 @@ def cli(classifier, catalog, entity, threshold, name_rule, upload, sandbox,
 
     rl.set_option(*constants.CLASSIFICATION_RETURN_SERIES)
 
-    for chunk in execute(
-            catalog, entity, model_path, name_rule, threshold, dir_io
+    for i, chunk in enumerate(
+            execute(model_path, catalog, entity, threshold, name_rule, dir_io)
     ):
         chunk.to_csv(result_path, mode='a', header=False)
 
         if upload:
-            _upload(chunk, catalog, entity, sandbox)
+            _upload(chunk, i, catalog, entity, sandbox)
 
     # Free memory in case of neural networks:
     # can be done only after classification
@@ -112,6 +112,104 @@ def cli(classifier, catalog, entity, threshold, name_rule, upload, sandbox,
         K.clear_session()  # Clear the TensorFlow graph
 
     LOGGER.info('Linking completed')
+
+
+def execute(model_path: str, catalog: str, entity: str, threshold: float,
+            name_rule: bool, dir_io: str) -> Iterator[pd.Series]:
+    """Run a supervised linker.
+
+    1. Build the classification set relevant to the given catalog and entity
+    2. generate links between Wikidata items and catalog identifiers
+
+    :param model_path: path to a trained model file
+    :param catalog: ``{'discogs', 'imdb', 'musicbrainz'}``.
+      A supported catalog
+    :param entity: ``{'actor', 'band', 'director', 'musician', 'producer',
+      'writer', 'audiovisual_work', 'musical_work'}``.
+      A supported entity
+    :param threshold: minimum confidence score for generated links.
+      Those below this value are discarded.
+      Must be a float between 0 and 1
+    :param name_rule: whether to enable the rule on full names or not:
+      if *True*, links with different full names
+      are discarded after classification
+    :param dir_io: input/output directory where working files
+      will be read/written
+    :return: the generator yielding chunks of links
+    """
+    goal = 'classification'
+    classifier = joblib.load(model_path)
+
+    # Wikidata side
+    wd_reader = workflow.build_wikidata(goal, catalog, entity, dir_io)
+    wd_generator = workflow.preprocess_wikidata(goal, wd_reader)
+
+    for i, wd_chunk in enumerate(wd_generator, 1):
+        # Collect samples via queries to the target DB
+        samples = blocking.full_text_query_block(
+            goal,
+            catalog,
+            wd_chunk[keys.NAME_TOKENS],
+            i,
+            target_database.get_main_entity(catalog, entity),
+            dir_io,
+        )
+
+        # Build target chunk from samples
+        target_reader = data_gathering.gather_target_dataset(
+            goal,
+            entity,
+            catalog,
+            set(samples.get_level_values(keys.TID)),
+        )
+
+        # Preprocess target chunk
+        target_chunk = workflow.preprocess_target(
+            goal, target_reader
+        )
+
+        # Extract features
+        features_path = os.path.join(
+            dir_io,
+            constants.FEATURES.format(catalog, entity, goal, i)
+        )
+        feature_vectors = workflow.extract_features(
+            samples, wd_chunk, target_chunk, features_path
+        )
+
+        # The classification set must have the same feature space
+        # as the training one
+        _add_missing_feature_columns(classifier, feature_vectors)
+
+        predictions = (
+            # LSVM doesn't support probability scores
+            classifier.predict(feature_vectors)
+            if isinstance(classifier, rl.SVMClassifier)
+            else classifier.prob(feature_vectors)
+        )
+
+        # Full name rule: if names differ, it's not a link
+        if name_rule:
+            LOGGER.info('Applying full names rule ...')
+            predictions = pd.DataFrame(predictions).apply(
+                _zero_when_different_names,
+                axis=1,
+                args=(wd_chunk, target_chunk),
+            )
+
+        # Wikidata URL rule: if the target ID has a Wikidata URL, it's a link
+        if target_chunk.get(keys.URL) is not None:
+            predictions = pd.DataFrame(predictions).apply(
+                _one_when_wikidata_link_correct, axis=1, args=(target_chunk,)
+            )
+
+        LOGGER.info('Chunk %d classified', i)
+
+        # Filter by threshold
+        above_threshold = predictions[predictions >= threshold]
+
+        # Remove duplicates
+        yield above_threshold[~above_threshold.index.duplicated()]
 
 
 def _handle_io(classifier, catalog, entity, dir_io):
@@ -148,100 +246,79 @@ def _handle_io(classifier, catalog, entity, dir_io):
     return model_path, result_path
 
 
-def _upload(predictions, catalog, entity, sandbox):
-    links = dict(predictions.to_dict().keys())
-    LOGGER.info('Starting addition of links to Wikidata ...')
+def _upload(chunk, chunk_number, catalog, entity, sandbox):
+    links = dict(chunk.to_dict().keys())
+
+    LOGGER.info(
+        'Starting upload of links to Wikidata, chunk %d ...',
+        chunk_number
+    )
+
     wikidata_bot.add_identifiers(links, catalog, entity, sandbox)
 
-
-def execute(
-    catalog: str,
-    entity: str,
-    model: str,
-    name_rule: bool,
-    threshold: float,
-    dir_io: str,
-) -> Iterator[pd.Series]:
-    classifier = joblib.load(model)
-
-    wd_reader = workflow.build_wikidata(
-        'classification', catalog, entity, dir_io
+    LOGGER.info(
+        'Upload to Wikidata completed, chunk %d',
+        chunk_number
     )
-    wd_generator = workflow.preprocess_wikidata('classification', wd_reader)
 
-    for i, wd_chunk in enumerate(wd_generator, 1):
-        samples = blocking.full_text_query_block(
-            'classification',
-            catalog,
-            wd_chunk[keys.NAME_TOKENS],
-            i,
-            target_database.get_main_entity(catalog, entity),
-            dir_io,
+
+def _add_missing_feature_columns(classifier, feature_vectors: pd.DataFrame):
+    # Handle amount of features depending on the classifier
+    if isinstance(classifier, rl.NaiveBayesClassifier):
+        # This seems to be the only easy way for Naïve Bayes
+        expected_features = len(classifier.kernel._binarizers)
+
+    elif isinstance(classifier, (classifiers.SVCClassifier, rl.SVMClassifier)):
+        expected_features = classifier.kernel.coef_.shape[1]
+
+    elif isinstance(classifier, (
+            neural_networks.SingleLayerPerceptron,
+            neural_networks.MultiLayerPerceptron
+        ),
+    ):
+        expected_features = classifier.kernel.input_shape[1]
+
+    else:
+        err_msg = (
+            f'Unsupported classifier: {classifier.__name__}. '
+            f'It should be one of {set(constants.CLASSIFIERS)}'
+        )
+        LOGGER.critical(err_msg)
+        raise ValueError(err_msg)
+
+    actual_features = feature_vectors.shape[1]
+
+    if expected_features != actual_features:
+        LOGGER.info(
+            'Feature vectors have %d features, but %s expected %d. '
+            'Will add missing ones',
+            actual_features,
+            classifier.__class__.__name__,
+            expected_features,
         )
 
-        # Build target chunk based on samples
-        target_reader = data_gathering.gather_target_dataset(
-            'classification',
-            entity,
-            catalog,
-            set(samples.get_level_values(keys.TID)),
-        )
-
-        # Preprocess target chunk
-        target_chunk = workflow.preprocess_target(
-            'classification', target_reader
-        )
-
-        # get features
-        features_path = os.path.join(
-            dir_io, constants.FEATURES % (catalog, entity, 'classification', i)
-        )
-
-        feature_vectors = workflow.extract_features(
-            samples, wd_chunk, target_chunk, features_path
-        )
-
-        _add_missing_feature_columns(classifier, feature_vectors)
-
-        predictions = (
-            classifier.predict(feature_vectors)
-            if isinstance(classifier, rl.SVMClassifier)
-            else classifier.prob(feature_vectors)
-        )
-
-        # See https://stackoverflow.com/a/18317089/10719765
-        if name_rule:
-            LOGGER.info('Applying full names rule ...')
-            predictions = pd.DataFrame(predictions).apply(
-                _zero_when_different_names,
-                axis=1,
-                args=(wd_chunk, target_chunk),
+        for i in range(expected_features - actual_features):
+            feature_vectors[f'missing_{i}'] = full(
+                len(feature_vectors), constants.FEATURE_MISSING_VALUE
             )
 
-        if target_chunk.get(keys.URL) is not None:
-            predictions = pd.DataFrame(predictions).apply(
-                _one_when_wikidata_link_correct, axis=1, args=(target_chunk,)
-            )
 
-        LOGGER.info('Chunk %d classified', i)
-
-        # get all 'confident' predictions
-        above_threshold = predictions[predictions >= threshold]
-
-        # yield only those predictions which are not duplicate
-        yield above_threshold[~above_threshold.index.duplicated()]
-
-
+# See https://stackoverflow.com/a/18317089/10719765
+# for `pandas` implementation details
 def _zero_when_different_names(prediction, wikidata, target):
     wd_names, target_names = set(), set()
     qid, tid = prediction.name
+
     for column in constants.NAME_FIELDS:
         if wikidata.get(column) is not None:
             values = wikidata.loc[qid][column]
+
             if values is not nan:
                 wd_names.update(set(values))
+
         if target.get(column) is not None:
             values = target.loc[tid][column]
+
             if values is not nan:
                 target_names.update(set(values))
 
@@ -250,54 +327,22 @@ def _zero_when_different_names(prediction, wikidata, target):
 
 def _one_when_wikidata_link_correct(prediction, target):
     qid, tid = prediction.name
-
     urls = target.loc[tid][keys.URL]
 
     if urls:
-        for u in urls:
-            if u:
-                if 'wikidata' in u:
-                    res = re.search(r'(Q\d+)$', u)
-                    if res:
-                        LOGGER.debug(
-                            f"""Changing prediction: {qid}, {tid} --- {u} = {1.0 if qid == res.groups()[
-                                0] else 0}, before it was {prediction[0]}"""
-                        )
-                        return 1.0 if qid == res.groups()[0] else 0
+        for url in urls:
+            if url and 'wikidata' in url:
+                has_qid = search(constants.QID_REGEX, url)
+
+                if has_qid:
+                    new_score = 1.0 if qid == has_qid.group() else 0.0
+
+                    LOGGER.debug(
+                        'Wikidata URL detected in %s target ID: %s '
+                        'Will update the confidence score from %f to %f',
+                        (qid, tid), url,
+                        prediction[0], new_score
+                    )
+                    return new_score
 
     return prediction[0]
-
-
-def _add_missing_feature_columns(classifier, feature_vectors: pd.DataFrame):
-    if isinstance(classifier, rl.NaiveBayesClassifier):
-        expected_features = len(classifier.kernel._binarizers)
-
-    elif isinstance(classifier, (classifiers.SVCClassifier, rl.SVMClassifier)):
-        expected_features = classifier.kernel.coef_.shape[1]
-
-    elif isinstance(
-            classifier,
-            (
-                    neural_networks.SingleLayerPerceptron,
-                    neural_networks.MultiLayerPerceptron,
-            ),
-    ):
-        expected_features = classifier.kernel.input_shape[1]
-
-    else:
-        err_msg = f'Unsupported classifier: {classifier.__name__}. It should be one of {set(constants.CLASSIFIERS)}'
-        LOGGER.critical(err_msg)
-        raise ValueError(err_msg)
-
-    actual_features = feature_vectors.shape[1]
-    if expected_features != actual_features:
-        LOGGER.info(
-            'Feature vectors have %d features, but %s expected %d. Will add missing ones',
-            actual_features,
-            classifier.__class__.__name__,
-            expected_features,
-        )
-        for i in range(expected_features - actual_features):
-            feature_vectors[f'missing_{i}'] = full(
-                len(feature_vectors), constants.FEATURE_MISSING_VALUE
-            )
