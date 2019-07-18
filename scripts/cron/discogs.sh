372bc154266d20d421362080e2897c030cf67e8d
Marco Fossati <fossati@fbk.eu>
2019-07-18 17:25:26 +0200

    shorter folder and tmux session names

--- scripts/cron/discogs.sh
+++ scripts/cron/discogs.sh
@@ -1,4 +1,4 @@
-cd /srv/soweego_production/soweego/
-/usr/bin/tmux kill-session -t pipeline-discogs
-/usr/bin/tmux new-session -d -s "pipeline-discogs"  
-/usr/bin/tmux send-keys -t pipeline-discogs:0 "./docker/launch_pipeline.sh -c ../prod_cred.json -s /srv/soweego_production/discogs-shared/ discogs --no-upload --validator" ENTER
+cd /srv/prod/soweego/
+/usr/bin/tmux kill-session -t discogs
+/usr/bin/tmux new-session -d -s discogs
+/usr/bin/tmux send-keys -t discogs:1 "./docker/pipeline.sh -c ../credentials.json -s /srv/prod/discogs/ discogs --no-upload --validator" ENTER
--- scripts/cron/imdb.sh
+++ scripts/cron/imdb.sh
@@ -1,4 +1,4 @@
-cd /srv/soweego_production/soweego/
-/usr/bin/tmux kill-session -t pipeline-imdb
-/usr/bin/tmux new-session -d -s "pipeline-imdb"
-/usr/bin/tmux send-keys -t pipeline-imdb:0 "./docker/launch_pipeline.sh -c ../prod_cred.json -s /srv/soweego_production/imdb-shared/ imdb --no-upload --validator" ENTER
+cd /srv/prod/soweego/
+/usr/bin/tmux kill-session -t imdb
+/usr/bin/tmux new-session -d -s imdb
+/usr/bin/tmux send-keys -t imdb:1 "./docker/pipeline.sh -c ../credentials.json -s /srv/prod/imdb/ imdb --no-upload --validator" ENTER
