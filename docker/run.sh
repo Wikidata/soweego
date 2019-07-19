851322e835a358dfe0939b13773d1b0a1ea2feb4
Hjfocs <hjfocs@soweego.soweego.eqiad.wmflabs>
2019-07-19 16:00:29 +0000

    remove hidden credentials option that was parsed & improve script

--- docker/run.sh
+++ docker/run.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 PROGNAME=$0
 
 usage() {
   cat << EOF >&2
-Usage: $PROGNAME [-s <dir>]
+Usage: $PROGNAME [-s DIRECTORY]
 
--s <dir>: ...
+-s DIRECTORY  Output directory. Default: /tmp/soweego_shared/
 
 EOF
   exit 1
@@ -13,18 +14,14 @@ EOF
 
 export DOCKER_SHARED_FOLDER="/tmp/soweego_shared/"
 
-while getopts :s:c: o; do
+while getopts :s: o; do
   case $o in
     (s) export DOCKER_SHARED_FOLDER="$OPTARG";;
-    (c) export CREDENTIALS_PATH="$OPTARG";;
     (*) usage
   esac
 done
 shift "$((OPTIND - 1))"
 
-if [[ -f "$CREDENTIALS_PATH" ]]; then
-    cp "${CREDENTIALS_PATH}" "${DOCKER_SHARED_FOLDER}/credentials.json"
-fi
-
 docker-compose -f "docker-compose.dev.yml" up -d --build
 docker-compose -f "docker-compose.dev.yml" exec soweego /bin/bash
+
