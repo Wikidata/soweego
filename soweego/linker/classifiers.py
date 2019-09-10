95eeb699f4f0c2f5908a3726e9fbe11abaa294c7
travis <travis@soweego.eqiad.wmflabs>
2019-09-10 19:08:23 +0000

    format code & organize imports

--- soweego/linker/classifiers.py
+++ soweego/linker/classifiers.py
@@ -209,12 +209,12 @@ class VoteClassifier(SKLearnAdapter, BaseClassifier):
 # shared across neural network implementations.
 class _BaseNeuralNetwork(KerasAdapter, BaseClassifier):
     def _fit(
-            self,
-            feature_vectors: pd.Series,
-            answers: pd.Series = None,
-            batch_size: int = constants.BATCH_SIZE,
-            epochs: int = constants.EPOCHS,
-            validation_split: float = constants.VALIDATION_SPLIT,
+        self,
+        feature_vectors: pd.Series,
+        answers: pd.Series = None,
+        batch_size: int = constants.BATCH_SIZE,
+        epochs: int = constants.EPOCHS,
+        validation_split: float = constants.VALIDATION_SPLIT,
     ) -> None:
         model_path = os.path.join(
             constants.SHARED_FOLDER,
@@ -303,9 +303,9 @@ class SingleLayerPerceptron(_BaseNeuralNetwork):
         self.kernel = model
 
     def _create_model(
-            self,
-            activation=constants.OUTPUT_ACTIVATION,
-            optimizer=constants.SLP_OPTIMIZER,
+        self,
+        activation=constants.OUTPUT_ACTIVATION,
+        optimizer=constants.SLP_OPTIMIZER,
     ):
         model = Sequential()
         model.add(Dense(1, input_dim=self.input_dim, activation=activation))
@@ -373,11 +373,11 @@ class MultiLayerPerceptron(_BaseNeuralNetwork):
         self.kernel = model
 
     def _create_model(
-            self,
-            optimizer=constants.SLP_OPTIMIZER,
-            hidden_activation=constants.HIDDEN_ACTIVATION,
-            output_activation=constants.OUTPUT_ACTIVATION,
-            hidden_layer_dims=constants.MLP_HIDDEN_LAYERS_DIM,
+        self,
+        optimizer=constants.SLP_OPTIMIZER,
+        hidden_activation=constants.HIDDEN_ACTIVATION,
+        output_activation=constants.OUTPUT_ACTIVATION,
+        hidden_layer_dims=constants.MLP_HIDDEN_LAYERS_DIM,
     ):
 
         model = Sequential()
