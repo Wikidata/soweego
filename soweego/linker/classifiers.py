d6a07623f85a739ed5dd5b6b44c601929504db9f
travis <travis@soweego.eqiad.wmflabs>
2019-09-11 11:47:30 +0000

    format code & organize imports

--- soweego/linker/classifiers.py
+++ soweego/linker/classifiers.py
@@ -209,12 +209,12 @@ class VoteClassifier(SKLearnAdapter, BaseClassifier):
 # shared across neural network implementations.
 class _BaseNeuralNetwork(KerasAdapter, BaseClassifier):
     def _fit(
-            self,
-            feature_vectors: pd.Series,
-            answers: pd.Series = None,
-            batch_size: int = None,
-            epochs: int = None,
-            validation_split: float = constants.VALIDATION_SPLIT,
+        self,
+        feature_vectors: pd.Series,
+        answers: pd.Series = None,
+        batch_size: int = None,
+        epochs: int = None,
+        validation_split: float = constants.VALIDATION_SPLIT,
     ) -> None:
 
         # if batch size or epochs have not been provided as arguments, and
@@ -317,9 +317,7 @@ class SingleLayerPerceptron(_BaseNeuralNetwork):
 
         self.kernel = model
 
-    def _create_model(
-            self, activation=None, optimizer=None
-    ):
+    def _create_model(self, activation=None, optimizer=None):
         if optimizer is None:
             optimizer = self.optimizer
 
@@ -392,11 +390,11 @@ class MultiLayerPerceptron(_BaseNeuralNetwork):
         self.kernel = model
 
     def _create_model(
-            self,
-            optimizer=constants.MLP_OPTIMIZER,
-            hidden_activation=constants.HIDDEN_ACTIVATION,
-            output_activation=constants.OUTPUT_ACTIVATION,
-            hidden_layer_dims=constants.MLP_HIDDEN_LAYERS_DIM,
+        self,
+        optimizer=constants.MLP_OPTIMIZER,
+        hidden_activation=constants.HIDDEN_ACTIVATION,
+        output_activation=constants.OUTPUT_ACTIVATION,
+        hidden_layer_dims=constants.MLP_HIDDEN_LAYERS_DIM,
     ):
 
         model = Sequential()
