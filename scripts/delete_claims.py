1f77e833b58090711bb8d185d5a8a8a0d29febfc
Marco at VPS <fossati@spaziodati.eu>
2021-10-12 10:49:49 +0000

    complete low-level edit workflow

--- scripts/delete_claims.py
+++ scripts/delete_claims.py
@@ -24,6 +24,7 @@ __version__ = '2.0'
 __license__ = 'GPL-3.0'
 __copyright__ = 'Copyleft 2021, Hjfocs'
 
+import json
 import sys
 
 import requests
@@ -33,11 +34,16 @@ STMT_PREFIX = 'http://www.wikidata.org/entity/statement/'
 
 
 def main(args):
-    if len(args) != 3:
-        print(f'Usage: python {__file__} GUIDS_CSV EDIT_SUMMARY')
+    if len(args) != 4:
+        print(f'Usage: python {__file__} GUIDS_CSV LOGIN_CREDENTIALS_JSON EDIT_SUMMARY')
         return 1
 
-    file_in, summary = args[1], args[2]
+    file_in, creds_in, summary = args[1:]
+
+    with open(creds_in) as fin:
+        creds = json.load(fin)
+    user = creds['WIKIDATA_API_USER']
+    pw = creds['WIKIDATA_API_PASSWORD']
     guids = set()
 
     with open(file_in) as fin:
@@ -50,28 +56,38 @@ def main(args):
 
     session = requests.Session()
 
+    # Get login token
+    params = {'action': 'query', 'meta': 'tokens', 'type': 'login', 'format': 'json'}
+    resp = session.get(WIKIDATA_API_URL, params=params)
+    login_token = resp.json()['query']['tokens']['logintoken']
+
+    # Log in
+    data = {'action': 'login', 'lgname': user, 'lgpassword': pw, 'lgtoken': login_token, 'format': 'json'}
+    resp = session.post(WIKIDATA_API_URL, data=data)
+
     # Get edit token
     params = {'action': 'query', 'meta': 'tokens', 'format': 'json'}
-    r = session.get(WIKIDATA_API_URL, params=params)
-    token = r.json()['query']['tokens']['csrftoken']
+    resp = session.get(WIKIDATA_API_URL, params=params)
+    edit_token = resp.json()['query']['tokens']['csrftoken']
 
     # Fire a POST for each GUID
     for guid in guids:
         data = {
             'action': 'wbremoveclaims',
             'format': 'json',
-            'token': token,
+            'token': edit_token,
             'bot': True,
             'claim': guid,
             'summary': summary,
         }
-        r = session.post(WIKIDATA_API_URL, data=data)
+        resp = session.post(WIKIDATA_API_URL, data=data)
 
-        if r.ok:
-            print(r.json())
+        if resp.ok:
+            print(resp.json())
 
-        return 0
+    return 0
 
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
+
